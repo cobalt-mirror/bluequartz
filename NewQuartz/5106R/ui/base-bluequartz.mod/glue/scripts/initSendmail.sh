7f2d0c4328791144fede0bdafb3eae24997cb793
Michael Stauber <mstauber@blueonyx.it>
2008-11-10 00:52:26 +0000

    Merged 5100R changes to 5106R codebase.

--- NewQuartz/5106R/ui/base-bluequartz.mod/glue/scripts/initSendmail.sh
+++ NewQuartz/5106R/ui/base-bluequartz.mod/glue/scripts/initSendmail.sh
@@ -5,12 +5,15 @@ if [ ! -f /etc/mail/aliases ]; then
   if [ -f /etc/mail/aliases.rpmsave ]; then
     mv /etc/mail/aliases.rpmsave /etc/mail/aliases
   else
-    cp -p $CONFDIR/aliases /etc/mail/aliases
+    ln -s /etc/aliases /etc/mail/aliases
+  fi
+  grep '^root:' /etc/mail/aliases > /dev/null 2>&1
+  if [ $? = 1 ]; then
+    echo 'root:		admin' >> /etc/mail/aliases
   fi
   /usr/sbin/makemap hash /etc/mail/aliases.db < /etc/mail/aliases
 fi
 cp -p $CONFDIR/sendmail.mc /etc/mail/sendmail.mc
-cp -p $CONFDIR/sendmail.pam /etc/pam.d/smtp.sendmail
 cp -p $CONFDIR/popauth.m4 /usr/share/sendmail-cf/hack/popauth.m4
 
 m4 /usr/share/sendmail-cf/m4/cf.m4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf
