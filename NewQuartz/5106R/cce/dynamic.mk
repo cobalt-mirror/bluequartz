cb591ea01588eaed9ac8f6e616dcf2592bf2bf45
Michael Stauber <mstauber@blueonyx.it>
2008-11-11 21:50:58 +0000

    Needed to bump version number from 0.80.2 to 0.80.3 to make it possible that virtual BlueQuartz VPS's can be updated as well.

--- NewQuartz/5106R/cce/dynamic.mk
+++ NewQuartz/5106R/cce/dynamic.mk
@@ -1,7 +1,7 @@
-CCE_TOPDIR=/home/devel/bluequartz/5106R/cce
+CCE_TOPDIR=/home/checkout.mstauber/trunk/NewQuartz/5106R/cce
 CCE_V_MAJ=0
 CCE_V_MIN=80
-CCE_V_PATCH=2
-CCE_VERSION=0.80.2
+CCE_V_PATCH=3
+CCE_VERSION=0.80.3
 include $(CCE_TOPDIR)/rules.mk
 CFLAGS += -fPIC
