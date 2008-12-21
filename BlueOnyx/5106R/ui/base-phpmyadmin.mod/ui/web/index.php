991c76e218e7968422d4b7c7da55375ead21cc0a
Michael Stauber <mstauber@blueonyx.it>
2008-12-21 04:02:12 +0000

    [1.0.0-0BO01] - German locales done. - ui/phpmyadmin/index.php based on the one for base-squirrelmail.mod - Requires phpMyAdmin - served out of the BlueOnyx repository. Oh boy. We're gonna regret this, as often as it updates. - Initial build for BlueOnyx.

---
+++ BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// phpMyAdmin Redirector
+// (C) Project BlueOnyx 2008 - All rights reserved.
+
+include_once("ServerScriptHelper.php");
+
+// Get login name:
+$serverScriptHelper = new ServerScriptHelper();
+$loginName = $serverScriptHelper->getLoginName();
+
+// Get FQDN of the server:
+$dn = $_SERVER['SERVER_NAME'];
+
+if ($_SERVER['HTTPS']) {
+    // User logged in by HTTPS - redirect to HTTPS URL:
+    header ("Location: https://$dn:81/phpMyAdmin/");
+}
+else {
+    // User logged in by HTTP - redirect to HTTP URL:
+    header ("Location: http://$dn:444/phpMyAdmin/");
+}
+
+?>
+
