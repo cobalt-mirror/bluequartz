51b125a9127dbf226640e0e2bc6fa75656bab1e6
Michael Stauber <mstauber@blueonyx.it>
2009-01-06 06:01:41 +0000

    [1.0.0-0BO04] - Added Rickard Osser's suggestion for automated logins for user admin.

--- BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/index.php
+++ BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/index.php
@@ -5,21 +5,42 @@
 
 include_once("ServerScriptHelper.php");
 
-// Get login name:
 $serverScriptHelper = new ServerScriptHelper();
 $loginName = $serverScriptHelper->getLoginName();
-
+ 
+$cceClient = $serverScriptHelper->getCceClient();
+ 
+if($loginName == "admin") {
+    $systemOid = $cceClient->getObject("System", array(), "mysql");
+    $loginName = $systemOid{'mysqluser'};
+    $mysqlOid = $cceClient->find("MySQL");
+    $mysqlData = $cceClient->get($mysqlOid[0]);
+    $sql_rootpassword = $mysqlData{'sql_rootpassword'};
+} 
+else {
+    $loginName = "";
+}
+ 
 // Get FQDN of the server:
 $dn = $_SERVER['SERVER_NAME'];
-
+ 
 if ($_SERVER['HTTPS']) {
-    // User logged in by HTTPS - redirect to HTTPS URL:
-    header ("Location: https://$dn:81/phpMyAdmin/");
+     // User logged in by HTTPS - redirect to HTTPS URL:
+    if($loginName) {
+	header ("Location: https://$loginName:$sql_rootpassword@$dn:81/phpMyAdmin/");
+    } 
+    else {
+	header ("Location: https://$dn:81/phpMyAdmin/");
+    }
 }
 else {
-    // User logged in by HTTP - redirect to HTTP URL:
-    header ("Location: http://$dn:444/phpMyAdmin/");
+     // User logged in by HTTP - redirect to HTTP URL:
+    if($loginName){
+	header ("Location: http://$loginName:$sql_rootpassword@$dn:444/phpMyAdmin/");
+    } 
+    else {
+	header ("Location: http://$dn:81/phpMyAdmin/");
+    }
 }
 
 ?>
-
