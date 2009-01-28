ce8af749deace0bd0649734e8056d4f89b067720
Rickard Osser <rickard.osser@bluapp.com>
2009-01-28 13:34:32 +0000

    Re-added automated logins for admin user, correctly implemented with single signon.

--- BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/index.php
+++ BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/index.php
@@ -3,16 +3,32 @@
 // phpMyAdmin Redirector
 // (C) Project BlueOnyx 2008 - All rights reserved.
 
-// Get FQDN of the server:
-$dn = $_SERVER['SERVER_NAME'];
+include_once("ServerScriptHelper.php");
 
-if ($_SERVER['HTTPS']) {
-    // User logged in by HTTPS - redirect to HTTPS URL:
-    header ("Location: https://$dn:81/phpMyAdmin/");
-}
-else {
-    // User logged in by HTTP - redirect to HTTP URL:
-    header ("Location: http://$dn:444/phpMyAdmin/");
-}
+$serverScriptHelper = new ServerScriptHelper();
+$loginName = $serverScriptHelper->getLoginName();
+$cceClient = $serverScriptHelper->getCceClient();
+
+if($loginName == "admin") {
+  $systemOid = $cceClient->getObject("System", array(), "mysql");
+  $loginName = $systemOid{'mysqluser'};
+  $mysqlOid = $cceClient->find("MySQL");
+  $mysqlData = $cceClient->get($mysqlOid[0]);
+  $sql_rootpassword = $mysqlData{'sql_rootpassword'};
+ } else {
+  $loginName = "";
+ }
+
+print "<form action='signon.php' method='post' name='frm' onLoad='document.frm.submit()'>";
+print "<input type='hidden' name='user' value='$loginName'>";
+print "<input type='hidden' name='password' value='$sql_rootpassword'>";
+print "<input type='hidden' name='host' value='localhost'>";
+print "<input type='image' name='' value=''>";
+print "</form>";
+print '
+<script language="JavaScript">
+       document.frm.submit();
+</script>
+';
 
 ?>
