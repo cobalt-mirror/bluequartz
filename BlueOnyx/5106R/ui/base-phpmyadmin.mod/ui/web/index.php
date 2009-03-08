7ddeea418b9a9934ceb74ae26c781819beed901b
Michael Stauber <mstauber@blueonyx.it>
2009-03-08 03:43:04 +0000

    [1.0.0-0BO08] - Reworked single signon so it now automatically fills in login details of siteAdmin's.

--- BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/index.php
+++ BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // phpMyAdmin Redirector
-// (C) Project BlueOnyx 2008 - All rights reserved.
+// (C) Bluapp AB and Project BlueOnyx 2009 - All rights reserved.
 
 include_once("ServerScriptHelper.php");
 
@@ -9,20 +9,53 @@ $serverScriptHelper = new ServerScriptHelper();
 $loginName = $serverScriptHelper->getLoginName();
 $cceClient = $serverScriptHelper->getCceClient();
 
-if($loginName == "admin") {
-  $systemOid = $cceClient->getObject("System", array(), "mysql");
-  $loginName = $systemOid{'mysqluser'};
-  $mysqlOid = $cceClient->find("MySQL");
-  $mysqlData = $cceClient->get($mysqlOid[0]);
-  $sql_rootpassword = $mysqlData{'sql_rootpassword'};
- } else {
+if ($loginName == "admin") {
+    $systemOid = $cceClient->getObject("System", array(), "mysql");
+    $db_username = $systemOid{'mysqluser'};
+    $mysqlOid = $cceClient->find("MySQL");
+    $mysqlData = $cceClient->get($mysqlOid[0]);
+    $db_pass = $mysqlData{'sql_rootpassword'};
+    $db_host = $mysqlData{'sql_host'};
+} 
+elseif ($serverScriptHelper->getAllowed('siteAdmin')) {
+    
+    // get user:
+    $oids = $cceClient->find("User", array("name" => $loginName));
+    $useroid = $oids[0];
+
+    // determine site he belongs to:
+    $user = $cceClient->get($oids[0]);
+    $group = $user["site"];
+
+    // Get MYSQL_Vsite settings for this site:
+    list($sites) = $cceClient->find("Vsite", array("name" => $group));
+    $MYSQL_Vsite = $cceClient->get($sites, 'MYSQL_Vsite');
+
+    // Fetch MySQL details for this site:
+    $db_enabled = $MYSQL_Vsite['enabled'];
+    $db_username = $MYSQL_Vsite['username'];
+    $db_pass = $MYSQL_Vsite['pass'];
+    $db_host = $MYSQL_Vsite['host'];
+
+    if ($db_enabled == "0") {
+        $db_host = "localhost";
+        $db_username = "";
+        $db_pass = "";
+    }
+}
+else {
   $loginName = "";
- }
+}
+
+// Sanity checks:
+if (!$db_host) {
+    $db_host = "localhost";
+}
 
 print "<form action='signon.php' method='post' name='frm' onLoad='document.frm.submit()'>";
-print "<input type='hidden' name='user' value='$loginName'>";
-print "<input type='hidden' name='password' value='$sql_rootpassword'>";
-print "<input type='hidden' name='host' value='localhost'>";
+print "<input type='hidden' name='PMA_user' value='$db_username'>";
+print "<input type='hidden' name='PMA_password' value='$db_pass'>";
+print "<input type='hidden' name='hostname' value='$db_host'>";
 print "<input type='image' name='' value=''>";
 print "</form>";
 print '
--- BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/signon.php
+++ BlueOnyx/5106R/ui/base-phpmyadmin.mod/ui/web/signon.php
@@ -1,7 +1,7 @@
 <?php
 
 // phpMyAdmin signon adapted for sausalito/Bluapp 
-// (C) Bluapp AB 2009 - All rights reserved.
+// (C) Bluapp AB and Project BlueOnyx 2009 - All rights reserved.
 
 include_once("ServerScriptHelper.php");
 include_once("uifc/Label.php");
@@ -14,48 +14,53 @@ $factory = $serverScriptHelper->getHtmlComponentFactory("base-phpmyadmin","/base
 
 $builder = new FormFieldBuilder();
 
-
-
+// Sanity checks:
+if ($db_enabled == "0") {
+	$db_host = "localhost";
+	$db_username = "";
+	$db_pass = "";
+}
+if (!$db_host) {
+	$db_host = "localhost";
+}
 
 /* Was data posted? */
-if ($_POST['user'] != "") {
-  if (isset($_POST['user'])) {
-    /* Need to have cookie visible from parent directory */
-    session_set_cookie_params(0, '/', '', 0);
-    /* Create signon session */
-    $session_name = 'SignonSession';
-    session_name($session_name);
-    session_start();
-    /* Store there credentials */
-    $_SESSION['PMA_single_signon_user'] = $_POST['user'];
-    $_SESSION['PMA_single_signon_password'] = $_POST['password'];
-    $_SESSION['PMA_single_signon_host'] = $_POST['host'];
-    $id = session_id();
-    /* Close that session */
-    session_write_close();
-    /* Redirect to phpMyAdmin (should use absolute URL here!) */
-    header('Location: /phpMyAdmin/index.php');
-  } 
- } else {
+if ($_POST['PMA_user'] != "") {
+    if (isset($_POST['PMA_user'])) {
+	/* Need to have cookie visible from parent directory */
+	session_set_cookie_params(0, '/', '', 0);
+	/* Create signon session */
+	$session_name = 'SignonSession';
+	session_name($session_name);
+	session_start();
+	/* Store there credentials */
+	$_SESSION['PMA_single_signon_user'] = $_POST['PMA_user'];
+	$_SESSION['PMA_single_signon_password'] = $_POST['PMA_password'];
+	$_SESSION['PMA_single_signon_host'] = $_POST['hostname'];
+	$id = session_id();
+	/* Close that session */
+	session_write_close();
+	/* Redirect to phpMyAdmin (should use absolute URL here!) */
+	header('Location: /phpMyAdmin/index.php');
+    } 
+} 
+else {
 
   $page = $factory->getPage();
   $block = $factory->getPagedBlock("PMA_logon");
   $block->addFormField(
-		       $factory->getTextField("user", ""),
+		       $factory->getTextField("PMA_user", $db_username),
 		       $factory->getLabel("PMA_user")
 		       );
-  $passwordField = $factory->getPassword("password");
+  $passwordField = $factory->getPassword("PMA_password", $db_pass);
   $passwordField->setConfirm(false);
   $block->addFormField(
 		       $passwordField,
 		       $factory->getLabel("PMA_password")
 		       );
-  $hidden = $builder->makeHiddenField("hostname", "localhost");
+  $hidden = $builder->makeHiddenField("hostname", $db_host);
 
   $block->addButton($factory->getButton($page->getSubmitAction(), "login"));
-
-
-  $serverScriptHelper->destructor();
 
   print($page->toHeaderHtml());
   print $hidden;
@@ -63,5 +68,6 @@ if ($_POST['user'] != "") {
   print($block->toHtml());
 	  
   print($page->toFooterHtml());
+  $serverScriptHelper->destructor();
 }
 ?>
