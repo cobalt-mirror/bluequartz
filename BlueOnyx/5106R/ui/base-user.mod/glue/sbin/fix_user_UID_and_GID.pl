a76c42a15d9d15aa329013b57fc35a91aa47915d
Michael Stauber <mstauber@blueonyx.it>
2009-06-23 22:21:26 +0000

    [1.2.0-0BQ26] - Added glue/sbin/fix_user_UID_and_GID.pl to allow manual fixing of UID and GID of user directories and logfile directories. - Modified glue/sbin/user_gid_fix.pl and added a root only check.

--- BlueOnyx/5106R/ui/base-user.mod/glue/sbin/fix_user_UID_and_GID.pl
+++ BlueOnyx/5106R/ui/base-user.mod/glue/sbin/fix_user_UID_and_GID.pl
@@ -1,21 +1,11 @@
 #!/usr/bin/perl
 #
-# $Id: user_gid_fix.pl, v 1.0.0.3 Tue 23 Jun 2009 06:17:01 PM EDT mstauber Exp $
+# $Id: fix_user_UID_and_GID.pl, v 1.0.0.3 Tue 23 Jun 2009 06:06:08 PM EDT mstauber Exp $
 # Copyright 2006-2009 Solarspeed Ltd. and NuOnce Networks, Inc. All rights reserved.
 #
-# During the mandatory PAM to shadow conversion we discovered that there were cases
-# where users were added to /etc/passwd with the default group "users". Which can 
-# cause problems with email forwarding and auto-responders. Aside from that all
-# users SHOULD be in the right group anyway.
-# 
-# Those problems during user creation have now been fixed. But this script allows
-# you to manually fix all existing users which still belong to group "users" 
-# (numerical GID = 100), but should NOT belong to it.
+# This script fixes the UID and GID of all Vsite users. Also fixed ownership of Vsite logs.
 #
-# Please note:
-#
-# User "admin" and server administrators rightfully belong to group "users". So this
-# script will NOT modify those, as they're fine.
+# Please note: This script does NOT change UID or GID of files in the webspace of a Vsite!
 #
 # Usage:
 #
@@ -36,10 +26,10 @@ if ($id ne "0") {
 }
 
 # Make backup copy of /etc/passwd - just in case:
-system("/bin/cp /etc/passwd /etc/.passwd.pre-uid-fix");
+system("/bin/cp /etc/passwd /etc/.passwd.pre-uid-fixing");
 
 # Give brief startup info:
-print "Checking for users with incorrect gid.\n";
+print "Processing UID/GID fix for all users.\n";
 
 $pw = new Unix::PasswdFile "/etc/passwd";
 $found_one = "0";
@@ -52,18 +42,12 @@ foreach $user ($pw->users) {
     $home = $pw->home($user);
     @groupworkaround = split(/\//, $home);
 
-    if (($uid >= "500") && ($gid == "100") && ($groupworkaround[2] eq ".sites")) {
+    if (($uid >= "500") && ($groupworkaround[2] eq ".sites")) {
 
-	# If this is not a system user AND he has a GID of 100 AND is not a serverAdmin,
-	# then this GID is most likely wrong. Determine real GID from parsing /etc/group:
+	# Determine GID of user by parsing /etc/group:
 	$grp = new Unix::GroupFile "/etc/group";
 	$real_gid_of_user = $grp->gid($groupworkaround[4]);
 	undef $grp;
-
-	# Fix all users with incorrect GID:
-	print "Fixing GID of $user ($uid), GID is: $gid, GID should be: $real_gid_of_user \n";
-	$pw->gid($user, $real_gid_of_user);
-	$pw->commit();
 
 	# Fixing GID of everything in the users home directory:
 	if (-d "$home") {
