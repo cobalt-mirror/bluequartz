2ac722d6f1757f8b19109e13780a532756c13370
Michael Stauber <mstauber@blueonyx.it>
2009-08-03 21:28:10 +0000

    [0.80.4-1BQ42] - Added pam_abl directory to include pam_abl by default - Modified server/system-auth-cce.pam to add pam_abl related rules - Added pam_abl.readme with some extra info into the build directory

--- BlueOnyx/5106R/cce/dynamic.mk
+++ BlueOnyx/5106R/cce/dynamic.mk
@@ -1,7 +1,7 @@
-CCE_TOPDIR=/home/checkout.mstauber/trunk/BlueOnyx/5106R/cce
+CCE_TOPDIR=/home/devel/BlueOnyx/5106R/cce
 CCE_V_MAJ=0
 CCE_V_MIN=80
-CCE_V_PATCH=3
-CCE_VERSION=0.80.3
+CCE_V_PATCH=4
+CCE_VERSION=0.80.4
 include $(CCE_TOPDIR)/rules.mk
 CFLAGS += -fPIC
