c513478c482f9116382a7c2dff23da612a9b673b
Michael Stauber <mstauber@blueonyx.it>
2011-04-18 04:39:05 +0000

    1.3.0-0BX13 - Updated constructor/set_httpd_alias_perms.pl to handle the CentOS welcome page for Apache and to stop and disable the unneeded avahi-daemon if present.

--- BlueOnyx/5106R/common/base-apache.mod/constructor/set_httpd_alias_perms.pl
+++ BlueOnyx/5106R/common/base-apache.mod/constructor/set_httpd_alias_perms.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w -I/usr/sausalito/perl -I.
-# $Id: set_httpd_alias_perms.pl Sun 10 Apr 2011 07:40:35 AM EDT mstauber $
+# $Id: set_httpd_alias_perms.pl Mon 18 Apr 2011 12:32:28 AM EDT mstauber $
 #
 # This constructor sets the GID and permissions on the databases in /etc/httpd/alias/
 # to the new requested standards as required by the mod_nss introduced by CentOS-5.6:
@@ -8,6 +8,18 @@
 if (-e "/etc/httpd/alias/cert8.db") {
         system('/usr/bin/find /etc/httpd/alias -user root -name "*.db" -exec /bin/chgrp apache {} \;');
         system('/usr/bin/find /etc/httpd/alias -user root -name "*.db" -exec /bin/chmod g+r {} \;');   
+}
+
+# While we are at it, delete the default CentOS welcome page:
+if (-e "/etc/httpd/conf.d/welcome.conf") {
+	system('/bin/rm -f /etc/httpd/conf.d/welcome.conf');
+}
+
+# A lot of BX servers have ImageMagick installed, which in turn installs and activates the avahi-daemon.
+# This daemon is not really needed and certainly should not be running. Hence we stop it and turn it off:
+if (-e "/etc/init.d/avahi-daemon") {
+	system('/etc/init.d/avahi-daemon stop >/dev/null 2>&1');
+	system('/sbin/chkconfig --level 2345 avahi-daemon off');
 }
 
 exit(0);
