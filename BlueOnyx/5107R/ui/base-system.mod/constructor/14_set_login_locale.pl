717e7675e07eb70c444b0d96fcc42d50d9d1d4a8
Michael Stauber <mstauber@blueonyx.it>
2011-07-12 14:06:45 +0000

    1.1.0-40BX26 - Added constructor/14_set_login_locale.pl to update /etc/sysconfig/i18n on CCEd restarts

---
+++ BlueOnyx/5107R/ui/base-system.mod/constructor/14_set_login_locale.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl -I/usr/sausalito/perl
+#
+# $Id: 14_set_login_locale.pl
+# 
+# Update /etc/sysconfig/i18n on CCEd restarts
+#
+
+use CCE;
+use Sauce::Util;
+
+# translation table to convert our generic locales to full glibc locales
+my $langs = {
+		'en' => 'en_US',
+		'de' => 'de_DE@euro',
+		'ja' => 'ja_JP.eucJP',
+		'es' => 'es_ES@euro',
+		'fr' => 'fr_FR@euro',
+		# these may be wrong, but I don't know chinese
+		'zh_CN' => 'zh_CN', 
+		'zh_TW' => 'zh_TW'
+	    };
+
+my $cce = new CCE;
+$cce->connectuds();
+
+# Find out which platform we're on:
+my ($fullbuild) = `cat /etc/build`;
+chomp($fullbuild);
+
+# figure out our product
+my ($product, $build, $lang) = ($fullbuild =~ m/^build (\S+) for a (\S+) in (\S+)/);
+
+# Get configured Language:
+(my $sys_oid) = $cce->find('System', '');
+($ok, $sys) = $cce->get($sys_oid);
+
+# Set locale to productLanguage:
+$locale = $sys->{productLanguage};
+
+Sauce::Util::editfile('/etc/sysconfig/i18n', *update_i18n, $locale);
+system("/bin/chmod 644 /etc/sysconfig/i18n");
+
+$cce->bye('SUCCESS');
+exit(0);
+
+sub update_i18n
+{
+	my ($in, $out, $locale) = @_;
+
+        my $locales = $sys->{"locales"};
+        $locales =~ s/^&//;     ##remove the front '&' before split
+        my @langlist = split('&', $locales);
+
+        #get the current lang code from "en_US" format
+        my $curlangcode = substr($sys->{productLanguage}, 0, 2);
+
+	# On 5107R and 5108R we need to switch from 'en' to 'en_US':
+	if (($build eq "5107R") || ($build eq "5108R")) {
+	    if ($curlangcode == "en") {
+		$curlangcode = "en_US";
+	    }
+	    # LC_ALL on SL6 must be a bit more specific:
+	    $locale_LC_ALL = "en_US.UTF-8";
+	}
+	else {
+	    # LC_ALL on CentOS5 can be a bit more simplicistic:
+	    $locale_LC_ALL = $locale;
+	}
+
+	#we need to move the current lang code to ahead of the rest
+	my $linguas = $curlangcode;
+	foreach my $tmp (@langlist) {
+		if ($tmp ne $curlangcode) {
+			$linguas = $linguas . " " . $tmp;
+		}
+	}
+
+	print $out <<LOCALE;
+LANG=$locale
+LC_ALL=$locale_LC_ALL
+LINGUAS="$linguas"
+LOCALE
+
+system("export LANG=$locale");
+system("export LC_ALL=$locale_LC_ALL");
+
+	return 1;
+}
+
