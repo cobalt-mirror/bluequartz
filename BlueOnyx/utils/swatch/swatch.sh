f6d8255a8269bedd301ebab35a3ae29cde6e8e6f
Michael Stauber <mstauber@blueonyx.it>
2011-08-20 21:21:28 +0000

    3.0.2-0BX03 - Modified /usr/sausalito/sbin/swatch.sh to prevent multiple execution of /usr/sbin/swatch

--- BlueOnyx/utils/swatch/swatch.sh
+++ BlueOnyx/utils/swatch/swatch.sh
@@ -2,4 +2,14 @@
 export LANG=en_US
 export LC_ALL=en_US.UTF-8
 export LINGUAS="en_US ja da_DK de_DE"
-/usr/sbin/swatch -c /etc/swatch.conf
+if [ -f "/tmp/.swatch.lock" ]; then
+        /bin/find /tmp/.swatch.lock -type f -cmin +25 -print | /usr/bin/xargs /bin/rm >/dev/null 2>&1
+        killall -9 /usr/sbin/swatch >/dev/null 2>&1
+        /etc/init.d/cced.init restart >/dev/null 2>&1
+else
+        /bin/touch /tmp/.swatch.lock
+        /usr/sbin/swatch -c /etc/swatch.conf
+        /bin/rm -f /tmp/.swatch.lock
+fi
+
+
