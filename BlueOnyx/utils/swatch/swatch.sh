2b5faa2e8a38fb0cce1f2935e2de790fda5f462f
Michael Stauber <mstauber@blueonyx.it>
2011-08-24 01:05:45 +0000

    3.0.2-0BX05 - Updated swatch.sh to use which to locate the binaries we need. Paths differ between OS's.

--- BlueOnyx/utils/swatch/swatch.sh
+++ BlueOnyx/utils/swatch/swatch.sh
@@ -5,26 +5,29 @@ export LC_ALL=en_US.UTF-8
 export LINGUAS="en_US ja da_DK de_DE"
 exec=/usr/sausalito/sbin/swatch.sh
 lockfile=/var/lock/subsys/swatch
+FIND=`which find`
+XARGS=`which xargs`
+TOUCH=`which touch`
+REM=`which rm`
 
 if [ -f $lockfile ] ; then
-	/bin/find $lockfile -type f -cmin +25 -print | /usr/bin/xargs /bin/rm >/dev/null 2>&1
-	#echo "Swatch cronjob is already running. Delaying execution for now.";
-	exit
+        $FIND $lockfile -type f -cmin +25 -print | $XARGS $REM >/dev/null 2>&1
+        #echo "Swatch cronjob is already running. Delaying execution for now.";
+        exit
 fi
 
-touch $lockfile
+$TOUCH $lockfile
 
 if [ -f "/tmp/.swatch.lock" ]; then
-        /bin/find /tmp/.swatch.lock -type f -cmin +25 -print | /usr/bin/xargs /bin/rm >/dev/null 2>&1
-	#echo "Swatch executeable is already running. Delaying execution for now.";
-	exit
+        $FIND /tmp/.swatch.lock -type f -cmin +25 -print | $XARGS $REM >/dev/null 2>&1
+        #echo "Swatch executeable is already running. Delaying execution for now.";
+        exit
 else
-        /bin/touch /tmp/.swatch.lock
+        $TOUCH /tmp/.swatch.lock
         /usr/sbin/swatch -c /etc/swatch.conf
-        /bin/rm -f /tmp/.swatch.lock
+        $REM -f /tmp/.swatch.lock
 fi
 
-/bin/rm -f $lockfile
+$REM -f $lockfile
 exit
-
 
