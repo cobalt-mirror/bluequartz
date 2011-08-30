21be2ddb2817b74b8ba2d90bd151cdf2266942de
Michael Stauber <mstauber@blueonyx.it>
2011-08-30 06:22:07 +0000

    0.1.2-87BX25 - Another fix for constructor/checkAM.pl to disable RAID on non-RAID systems.

--- BlueOnyx/ui/base-raid.mod/constructor/checkAM.pl
+++ BlueOnyx/ui/base-raid.mod/constructor/checkAM.pl
@@ -2,23 +2,45 @@
 #
 # Copyright (c) 2000 Cobalt Networks
 # Copyright (c) 2004 Turbolinux, inc.
+# Copryight (c) 2011 Team BlueOnyx
 
 # disables AM on non-RAID system
 
 use CCE;
 $cce = new CCE;
 
-if (! -e '/proc/mdstat') {
-    $on = "0";
+$mdstat = "/proc/mdstat";
+
+# Start sane - assume no RAID:
+$on = "0";
+
+if ( -e $mdstat) {
+    open(DATA, $mdstat) || die("Could not open $mdstat!");
+    @raw_data=<DATA>;
+    close(DATA);
+
+    $foundraid = "0";
+
+    foreach $line (@raw_data) {
+        chomp($line);
+        if ($line =~ /^md(.*)/ ) {
+                $foundraid++;
+        }
+    }
+
+    if ($foundraid > "0") {
+        $on = "1";
+    }
 }
 else {
-    $on = "1";
+    # No RAID:
+    $on = "0";
 }
 
 $cce->connectuds();
 my @oids = $cce->find ('ActiveMonitor');
 if ($#oids > -1) {
-  $cce->set($oids[0], 'DiskIntegrity', { "monitor" => $on });
+    $cce->set($oids[0], 'DiskIntegrity', { "monitor" => $on });
 }
 $cce->bye();
 exit 0;
