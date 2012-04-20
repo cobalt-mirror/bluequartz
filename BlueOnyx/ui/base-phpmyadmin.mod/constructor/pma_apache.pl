44d98a9e347e5bec73dad4219686ca46fbae2038
Michael Stauber <mstauber@blueonyx.it>
2012-04-20 11:07:33 +0000

    1.1.0-0BX14 - Modified constructor/pma_apache.pl

--- BlueOnyx/ui/base-phpmyadmin.mod/constructor/pma_apache.pl
+++ BlueOnyx/ui/base-phpmyadmin.mod/constructor/pma_apache.pl
@@ -25,23 +25,30 @@ $cce->connectuds();
 if ( -f $sample_conf )  {
     # Parse phpMyAdmin.conf to see if it still has the open_basedir directive in it:
     if ( -f $apache_conf )  {
-	$CHECK_CONFIG = `/bin/cat /etc/httpd/conf.d/squirrelmail.conf | /bin/grep open_basedir | /usr/bin/wc -l`; 
-	chomp($CHECK_CONFIG_AGAIN);
-	$CHECK_CONFIG_AGAIN = `/bin/cat /etc/httpd/conf.d/squirrelmail.conf | /bin/grep safe_mode_include_dir | /usr/bin/wc -l`; 
-	chomp($CHECK_CONFIG_AGAIN);
+        $CHECK_CONFIG = `/bin/cat /etc/admserv/conf.d/phpMyAdmin.conf | /bin/grep open_basedir | /usr/bin/wc -l`; 
+        chomp($CHECK_CONFIG);
+        if ( -e $apache_conf )  {
+                $CHECK_CONFIG_AGAIN = `/bin/cat /etc/httpd/conf.d/phpMyAdmin.conf | /bin/grep safe_mode_include_dir | /usr/bin/wc -l`; 
+                chomp($CHECK_CONFIG_AGAIN);
+        }
+        else {
+                $CHECK_CONFIG_AGAIN = "1";
+        }
     }
 
+print "1: $CHECK_CONFIG - $CHECK_CONFIG_AGAIN \n";
+
     # On bad results copy the "good" squirrelmail.conf from AdmServ over to the public Apache:
-    unless (($CHECK_CONFIG == "2") && ($CHECK_CONFIG_AGAIN == "2")) {
-	if ( -e $admserv_conf ) {
-	    # Copy config over:
-	    system("/bin/cp $sample_conf $admserv_conf");
-	    if ( -f $apache_conf )  {
-		system("/bin/cp $sample_conf $apache_conf");
-		# Reload Apache:
-		Sauce::Service::service_run_init('httpd', 'reload');
-	    }
-	}
+    unless (($CHECK_CONFIG == "1") && ($CHECK_CONFIG_AGAIN == "1")) {
+        if ( -e $admserv_conf ) {
+            # Copy config over:
+            system("/bin/cp $sample_conf $admserv_conf");
+            if ( -f $apache_conf )  {
+                system("/bin/cp $sample_conf $apache_conf");
+                # Reload Apache:
+                Sauce::Service::service_run_init('httpd', 'reload');
+            }
+        }
     }
 }
 
