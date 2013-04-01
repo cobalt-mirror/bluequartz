6a6c0d0821969b9ae3c1dbcbbe8f8af6092d67bc
Michael Stauber <mstauber@blueonyx.it>
2013-04-01 23:22:43 +0000

    1.1.0-94BX03 - Updated glue/sbin/named_trigger_update.pl to really set new default   DNS server settings. Relying alone on the Schema file is not good enough.

--- BlueOnyx/ui/base-dns.mod/glue/sbin/named_trigger_update.pl
+++ BlueOnyx/ui/base-dns.mod/glue/sbin/named_trigger_update.pl
@@ -2,7 +2,8 @@
 # $Id: named_trigger_update.pl,v Di 02 Apr 2013 00:37:48 CEST mstauber Exp $
 # Copyright 2013 Team BlueOnyx, All rights reserved.
 # 
-# Forces an update of the /etc/named.conf configuration.
+# Forces an update of the /etc/named.conf configuration and pushes out new 
+# (safe) default settings for the DNS server:
 
 use CCE;
 my $cce = new CCE;
@@ -12,10 +13,16 @@ my @sysoids = $cce->find('System');
 die "Could not find System object in CCE, named initialization failed.\n"
 	unless ($sysoids[0] =~ /^\d+$/);
 
-my($ok) = $cce->set($sysoids[0], 'DNS', { 'commit' => time(), 'dirty' => "1" });
+my($ok) = $cce->set($sysoids[0], 'DNS', { 
+	'query' => '1',
+	'query_all_allowed' => '1', 
+	'query_inetaddr' => '127.0.0.1/32', 
+	'caching' => '1',
+	'caching_all_allowed' => '0',
+	'recursion_inetaddr' => '127.0.0.1/32',
+	'commit' => time(), 'dirty' => "1" });
 
 
 die "Could not enable DNS service from CCE, named initialization failed.\n"
 	unless ($sysoids[0] =~ /^\d+$/);
-
 
