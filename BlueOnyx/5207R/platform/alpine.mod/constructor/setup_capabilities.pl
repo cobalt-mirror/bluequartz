2add012e02f4b9c65c0185739e305f72229b10d9
Michael Stauber <mstauber@blueonyx.it>
2013-12-03 22:45:16 +0000

    0.2-1BX01 - Preparational build for 5207R/5208R. Doesn't include new GUI yet. - Platform strings still set to 5107R/5108R at this time. - Cleaned out 'REQUIRED' list in Makefile. - Merged in new locales from 5207R (es_ES, fr_FR, it_IT, pt_PT). - Dropped all two character locales. - Converted ja_JP from EUC-JP to UTF-8. - Merged in new capabilities from Hisao Shibuya's 5200R code branch. - Modified web/intro.html.ja from EUC-JP to UTF-8.

--- BlueOnyx/5207R/platform/alpine.mod/constructor/setup_capabilities.pl
+++ BlueOnyx/5207R/platform/alpine.mod/constructor/setup_capabilities.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
-# Copyright 2001 Sun Microsystems, Inc.  All rights reserved.
-# $Id: setup_capabilities.pl,v 1.6 2001/11/15 22:02:36 pbaltz Exp $
+# $Id: setup_capabilities.pl
 # setup all necessary capability groups for the product
 # this maybe should go elsewhere or the really detailed stuff should 
 # be read in, maybe?
@@ -14,71 +13,79 @@ $cce->connectuds();
 
 # create all minor groups
 my @groups = (
-                {
-                    'name' => 'powerOptions',
-                    'shown' => 1,
-                    'capabilities' => $cce->array_to_scalar(
-                                        'controlPower')
-                },
-                {
-                    'name' => 'serverBackup',
-                    'shown' => 1,
-                    'capabilities' => $cce->array_to_scalar(
-					'modifyArkeia', 'modifyNetWorker',
-					'modifyNetBackup')
-                },
-                {
-                    'name' => 'systemMonitor',
-                    'shown' => 1,
-                    'capabilities' => ''
-                },
-                {
-                    'name' => 'networkServices',
-                    'shown' => 1,
-                    'capabilities' => $cce->array_to_scalar(
-                                        'modifyEmail', 'modifyFtp',
-                                        'modifyTelnet', 'modifyHttpd',
-					'modifySnmp')
-                },
-                {
-                    'name' => 'webServices',
-                    'shown' => 1,
-                    'capabilities' => $cce->array_to_scalar(
-                                        'modifyAsp', 'modifyJava')
-                },
-                {
-                    'name' => 'serverConfig',
-                    'shown' => 1,
-                    'capabilities' => ''
-                },
-                {
-                    'name' => 'siteAdmin',
-                    'shown' => 1,
-                    'capabilities' => ''
-                },
-		{
-		    'name' => 'dnsAdmin',
-		    'shown' => 1,
-		    'capabilities' => $cce->array_to_scalar(
-					'modifyDNS')
-		},
+        {
+            'name' => 'menuServerNetworkServices',
+            'shown' => 1,
+            'capabilities' => $cce->array_to_scalar(
+                    'serverHttpd', 'serverFTP',
+                    'serverEmail', 'serverDNS',
+                    'serverSNMP', 'serverShell',
+                    'serveriStat'
+                    )
+        },
+        {
+            'name' => 'menuServerSecurity',
+            'shown' => 1,
+            'capabilities' => $cce->array_to_scalar(
+                    'serverSSL', 'serverSystemFirewall')
+        },
+        {
+            'name' => 'menuServerSystem',
+            'shown' => 1,
+            'capabilities' => $cce->array_to_scalar(
+                    'serverNetwork', 'serverIpPooling',
+                    'serverVsite', 'serverPower',
+                    'serverTime', 'serverInformation')
+        },
+        {
+            'name' => 'menuServerMaintenance',
+            'shown' => 1,
+            'capabilities' => $cce->array_to_scalar(
+                    'serverServerDesktop')
+        },
+        {
+            'name' => 'menuServerServerStats',
+            'shown' => 1,
+            'capabilities' => $cce->array_to_scalar(
+                    'serverStatsServerNetwork',
+                    'serverStatsServerWebalizer',
+                    'serverStatsServerWeb',
+                    'serverStatsServerFTP',
+                    'serverStatsServerEmail',
+                    'serverStatsServerDisk')
+        },
+        {
+            'name' => 'menuServerActiveMonitor',
+            'shown' => 1,
+            'capabilities' => $cce->array_to_scalar(
+                    'serverShowActiveMonitor',
+                    'serverActiveMonitor')
+        },
+        {
+            'name' => 'serverManage',
+            'shown' => 1,
+            'capabilities' => $cce->array_to_scalar(
+                    'menuServerNetworkServices',
+                    'menuServerSecurity',
+                    'menuServerSystem',
+                    'menuServerMaintenance',
+                    'menuServerServerStatus',
+                    'menuServerActiveMonitor')
+        },
+        {
+            'name' => 'siteAdmin',
+            'shown' => 1,
+            'capabilities' => ''
+        },
                 {
                     'name' => 'adminUser',
                     'shown' => 1,
                     'capabilities' => $cce->array_to_scalar(
-                                        'siteAdmin', 'adminBlueLinq',
-                                        'serverBackup', 'systemMonitor',
-                                        'networkServices', 'webServices',
-                                        'serverConfig', 'siteFrontpage',
-                                        'siteSSL',
-                                        'siteAnonFTP', 'siteShell',
-                                        'modifySystemTime', 'scanDetection',
-					'overflow',
-					'dnsAdmin'
+                                        'siteAdmin', 'siteSSL',
+                                        'siteAnonFTP', 'siteShell'
                                       )
                 }
             );
-
 
 # now loop through all the capability groups and create as necessary
 for my $cap (@groups)
@@ -106,17 +113,20 @@ for my $cap (@groups)
 
 $cce->bye('SUCCESS');
 exit(0);
+
+#
+# Copyright (c) 2013 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2013 Team BlueOnyx, BLUEONYX.IT
 # Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
 # 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
 # 
-# -Redistribution of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
+# -Redistribution of source code must retain the above copyright notice, this  list of conditions and the following disclaimer.
 # 
 # -Redistribution in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation  
-# and/or other materials provided with the distribution.
+# this list of conditions and the following disclaimer in the documentation and/or 
+# other materials provided with the distribution.
 # 
 # Neither the name of Sun Microsystems, Inc. or the names of contributors may 
 # be used to endorse or promote products derived from this software without 
@@ -125,3 +135,4 @@ exit(0);
 # This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
 # 
 # You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# 
