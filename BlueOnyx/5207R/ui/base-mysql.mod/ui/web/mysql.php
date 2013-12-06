963086bcfa04558681c8253e9de00e914f2e8637
Michael Stauber <mstauber@blueonyx.it>
2013-12-06 22:40:50 +0000

    2.1.0-0BX01 - Preparational build for 5207R/5208R. Doesn't include new GUI yet. - Updated Danish, English and German locales. - Merged in new locales from 5207R (es_ES, fr_FR, it_IT, pt_PT). - Dropped all two character locales. - Converted ja_JP from EUC-JP to UTF-8. - Updated Schema files with new additions from Hisao's 5200R tree. - Added Hideki Oride's glue/handlers/exist_user_and_db_check.pl and modified it to use localized error messages. - Added localized error messages from glue/handlers/exist_user_and_db_check.pl to all locales. - Modified MySQL version check in ui/web/mysqlusers.php

--- BlueOnyx/5207R/ui/base-mysql.mod/ui/web/mysql.php
+++ BlueOnyx/5207R/ui/base-mysql.mod/ui/web/mysql.php
@@ -1,10 +1,9 @@
 <?php
 	// Authors: Brian N. Smith and Michael Stauber
-	// Copyright 2006, NuOnce Networks, Inc. and Solarspeed Ltd. All rights reserved.
 	// This is a quick and very dirty merge of the NuOnce MySQL and the Solarspeed MySQL modules.
 	// There are quite a few redundancies here. Wonder if a rewrite from scratch might have been 
 	// better instead. :o/ But for now it works.
-	// $Id: mysql.php,v 2.2 Sun Tue 09 Jun 2009 08:56:28 PM EDT mstauber Exp $
+	// $Id: mysql.php
 
 	function format_bytes ( $size ) {
 		switch ( $size ) {
@@ -257,5 +256,28 @@
 	echo $page->toFooterHtml();
 
 	$serverScriptHelper->destructor();
+
+/*
+Copyright (c) 2013 Michael Stauber, SOLARSPEED.NET
+Copyright (c) 2013 Team BlueOnyx, BLUEONYX.IT
+Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+-Redistribution of source code must retain the above copyright notice, this  list of conditions and the following disclaimer.
+
+-Redistribution in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Sun Microsystems, Inc. or the names of contributors may 
+be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+
+You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+*/
 
 ?>
--- BlueOnyx/5207R/ui/base-mysql.mod/ui/web/mysqlusers.php
+++ BlueOnyx/5207R/ui/base-mysql.mod/ui/web/mysqlusers.php
@@ -1,7 +1,6 @@
 <?php
 
 // Author: Michael Stauber <mstauber@solarspeed.net>
-// Copyright 2006-2008, Stauber Multimedia Design. All rights reserved.
 
 include_once("ServerScriptHelper.php");
 
@@ -127,12 +126,12 @@ $block->addFormField(
 $block->addDivider($factory->getLabel("DIVIDER_THREE", false), 'MySQL_TAB_ONE');
 
 $mysql_so_15 = '/usr/lib/mysql/libmysqlclient.so.15.0.0';
-$mysql_so_16 = '/usr/lib/mysql/libmysqlclient.so.16.0.0';
-if ((file_exists($mysql_so_15)||($mysql_so_16))) {
-	$access = 'rw';
+if (file_exists($mysql_so_15)) {
+  // MySQLd is too old, so we set this to read only:
+	$access = 'r';
 }
 else {
-	$access = 'r';
+	$access = 'rw';
 }
 
 $CREATE_VIEW = $systemObj['CREATE_VIEW'];
@@ -223,4 +222,27 @@ function br2nl($str) {
    $str = preg_replace("/(\r\n|\n|\r)/", "", $str);
    return preg_replace("=<br */?>=i", "\n", $str);
 }
+
+/*
+Copyright (c) 2013 Michael Stauber, SOLARSPEED.NET
+Copyright (c) 2013 Team BlueOnyx, BLUEONYX.IT
+Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+-Redistribution of source code must retain the above copyright notice, this  list of conditions and the following disclaimer.
+
+-Redistribution in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation and/or 
+other materials provided with the distribution.
+
+Neither the name of Sun Microsystems, Inc. or the names of contributors may 
+be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+
+You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+*/
 ?>
