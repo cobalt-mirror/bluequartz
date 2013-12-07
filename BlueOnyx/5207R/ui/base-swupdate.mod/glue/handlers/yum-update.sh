ff3f9117db44ed5bb6dc4788038e5ba06af7947a
Michael Stauber <mstauber@blueonyx.it>
2013-12-07 16:52:10 +0000

    1.4.1-0BX02 - Added a 'yum groupinstall blueonyx' to glue/handlers/yum-update.sh

--- BlueOnyx/5207R/ui/base-swupdate.mod/glue/handlers/yum-update.sh
+++ BlueOnyx/5207R/ui/base-swupdate.mod/glue/handlers/yum-update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Author: Brian N. Smith, Michael Stauber 
-# Copyright 2006-2007, NuOnce Networks, Inc.  All rights reserved. 
-# Copyright 2006-2007, Stauber Multimedia Design  All rights reserved. 
-# $Id: yum-update.sh, v1.0 2007/12/20 9:02:00 Exp $   
+# $Id: yum-update.sh
 
 /bin/touch /var/log/yum.log
 /bin/chmod 644 /var/log/yum.log
@@ -16,6 +14,8 @@ if [ -f /etc/yumgui.conf ]; then
   /bin/cat /tmp/yum.update | /bin/sed 's/\r//' | /bin/mail -s "`/bin/hostname` Yum Update output for `/bin/date +\%m`-`/bin/date +\%d`-`/bin/date +\%y`" $EMAILRECIPIENT
 fi
 
+/usr/bin/yum --exclude=base-maillist* --exclude=majordomo -y groupinstall blueonyx
+
 /usr/bin/yum check-update > /tmp/yum.check-update
 /bin/rm -f /tmp/yum.updating
 
@@ -23,3 +23,25 @@ fi
 
 exit 0;
 
+# 
+# Copyright (c) 2013 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2013 Team BlueOnyx, BLUEONYX.IT
+# Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+# 
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+# 
+# -Redistribution of source code must retain the above copyright notice, this  list of conditions and the following disclaimer.
+# 
+# -Redistribution in binary form must reproduce the above copyright notice, 
+# this list of conditions and the following disclaimer in the documentation and/or 
+# other materials provided with the distribution.
+# 
+# Neither the name of Sun Microsystems, Inc. or the names of contributors may 
+# be used to endorse or promote products derived from this software without 
+# specific prior written permission.
+# 
+# This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# 
+# You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# 
