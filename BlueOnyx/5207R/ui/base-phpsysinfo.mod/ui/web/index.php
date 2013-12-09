48a102d556e44c648c6618d10b3e059ca95fd6f7
Michael Stauber <mstauber@blueonyx.it>
2013-12-09 03:00:50 +0000

    1.2.0-0BX02 - Changed required capabilities to 'serverServerDesktop' to prevent resellers from using phpSysinfo.

--- BlueOnyx/5207R/ui/base-phpsysinfo.mod/ui/web/index.php
+++ BlueOnyx/5207R/ui/base-phpsysinfo.mod/ui/web/index.php
@@ -4,6 +4,12 @@ include_once("ServerScriptHelper.php");
 
 $serverScriptHelper = new ServerScriptHelper();
 
+// Only 'serverServerDesktop' should be here
+if (!$serverScriptHelper->getAllowed('serverServerDesktop')) {
+  header("location: /error/forbidden.html");
+  return;
+}
+
 header("location: /base/phpsysinfo/.phpsysinfo/index.php");
 return;
 
