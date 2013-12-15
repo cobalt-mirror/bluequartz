f4e4587140396c4af7fc9be506277caeaf06389b
Michael Stauber <mstauber@blueonyx.it>
2013-12-15 16:15:10 +0000

    1.2.0-0BX03 - Modified glue/sbin/named_trigger_update.pl to only run if initial setup   has not yet been completed. We really don't want to run this on boxes   that already have been configured, as it might break expected DNS   behaviour.

--- BlueOnyx/5207R/ui/base-dns.mod/glue/sbin/named_trigger_update.pl
+++ BlueOnyx/5207R/ui/base-dns.mod/glue/sbin/named_trigger_update.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -I /usr/sausalito/perl
-# $Id: named_trigger_update.pl,v Di 02 Apr 2013 00:37:48 CEST mstauber Exp $
+# $Id: named_trigger_update.pl
 # Copyright 2013 Team BlueOnyx, All rights reserved.
 # 
 # Forces an update of the /etc/named.conf configuration and pushes out new 
@@ -13,16 +13,34 @@ my @sysoids = $cce->find('System');
 die "Could not find System object in CCE, named initialization failed.\n"
 	unless ($sysoids[0] =~ /^\d+$/);
 
-my($ok) = $cce->set($sysoids[0], 'DNS', { 
-	'query' => '1',
-	'query_all_allowed' => '1', 
-	'query_inetaddr' => '127.0.0.1/32', 
-	'caching' => '1',
-	'caching_all_allowed' => '0',
-	'recursion_inetaddr' => '127.0.0.1/32',
-	'commit' => time(), 'dirty' => "1" });
+my @oids = $cce->find('System');
+if (not @oids) {
+        $cce->bye('FAIL');
+        exit 1;
+}
+
+$firstboot = "0";
+my ($ok, $obj) = $cce->get($oids[0]);
+if ($obj->{isLicenseAccepted} == "0") {
+    $firstboot = "1";
+}
+
+if ($firstboot == "1") {
+
+	my($ok) = $cce->set($sysoids[0], 'DNS', { 
+		'query' => '1',
+		'query_all_allowed' => '1', 
+		'query_inetaddr' => '127.0.0.1/32', 
+		'caching' => '1',
+		'caching_all_allowed' => '0',
+		'recursion_inetaddr' => '127.0.0.1/32',
+		'commit' => time(), 'dirty' => "1" });
 
 
-die "Could not enable DNS service from CCE, named initialization failed.\n"
-	unless ($sysoids[0] =~ /^\d+$/);
+	die "Could not enable DNS service from CCE, named initialization failed.\n"
+		unless ($sysoids[0] =~ /^\d+$/);
 
+}
+
+$cce->bye('SUCCESS');
+exit(0);
