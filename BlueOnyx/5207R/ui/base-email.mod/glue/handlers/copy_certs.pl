2d7681c3c6cdd0b2fea398b6884ee9d5777174fe
Michael Stauber <mstauber@blueonyx.it>
2014-01-28 17:20:38 +0000

    1.6.0-0BX05 - Modified glue/handlers/copy_certs.pl and glue/conf/email.conf as email   related services were not restarted on SSL certificate updates.

--- BlueOnyx/5207R/ui/base-email.mod/glue/handlers/copy_certs.pl
+++ BlueOnyx/5207R/ui/base-email.mod/glue/handlers/copy_certs.pl
@@ -11,14 +11,14 @@ $cce->connectfd();
 # for smtps
 system("cat /etc/admserv/certs/key /etc/admserv/certs/certificate > /usr/share/ssl/certs/sendmail.pem");
 chmod 0600, "/usr/share/ssl/certs/sendmail.pem";
-Sauce::Service::service_run_init('sendmail', 'restart') if $obj->{enableSMTPS};
+Sauce::Service::service_run_init('sendmail', 'restart');
 
 
 # for dovecot
 system("/bin/cp /etc/admserv/certs/key /etc/pki/dovecot/private/dovecot.pem");
 system("/bin/cp /etc/admserv/certs/certificate /etc/pki/dovecot/certs/dovecot.pem");
 
-Sauce::Service::service_run_init('dovecot', 'restart') if $obj->{enableIMAPS} || $obj->{enablePOPS};
+Sauce::Service::service_run_init('dovecot', 'restart');
 
 $cce->bye("SUCCESS");
 exit(0);
