357003fe88abf935504a9b5865d5aef7a1e5c4be
Michael Stauber <mstauber@blueonyx.it>
2014-03-27 16:51:57 +0000

    Fix for Japanese vacation messages.

--- BlueOnyx/ui/base-user.mod/ui/web/personalEmail.php
+++ BlueOnyx/ui/base-user.mod/ui/web/personalEmail.php
@@ -1,7 +1,6 @@
 <?php
-// Author: Kevin K.M. Chiu
-// Copyright 2000, Cobalt Networks.  All rights reserved.
-// $Id: personalEmail.php 1136 2008-06-05 01:48:04Z mstauber $
+// Author: Kevin K.M. Chiu & Michael Stauber
+// $Id: personalEmail.php
 
 include_once("ServerScriptHelper.php");
 include_once("uifc/PagedBlock.php");
@@ -14,6 +13,7 @@ $loginName = $serverScriptHelper->getLoginName();
 
 // get setting
 $userEmail = $cceClient->getObject("User", array("name" => $loginName), "Email");
+$User = $cceClient->getObject("User", array("name" => $loginName), "");
 
 $page = $factory->getPage();
 
@@ -62,12 +62,26 @@ $enableAutoResponder->addFormField($factory->getTimeStamp("oldStop", $oldStop, "
 $enableAutoResponder->addFormField($autoRespondStartDate, $factory->getLabel("autoRespondStartDate")); 
 $enableAutoResponder->addFormField($autoRespondStopDate, $factory->getLabel("autoRespondStopDate")); 
 
-$enableAutoResponder->addFormField(
-  $factory->getTextBlock("autoResponderMessageField", BXEncoding::toWin1252($userEmail["vacationMsg"])),
-  $factory->getLabel("autoResponderMessageField")
-);
+if ($User['localePreference'] != "ja_JP") {
+  $enableAutoResponder->addFormField(
+    $factory->getTextBlock("autoResponderMessageField", BXEncoding::toWin1252($userEmail["vacationMsg"])),
+    $factory->getLabel("autoResponderMessageField")
+  );
+}
+else {
+  $enableAutoResponder->addFormField(
+    $factory->getTextBlock("autoResponderMessageField", EncodingConv::doJapanese($userEmail["vacationMsg"], "euc")),
+    $factory->getLabel("autoResponderMessageField")
+  );
+}
+
 $autoResponder->addOption($enableAutoResponder);
 $block->addFormField($autoResponder, $factory->getLabel("autoResponderField"));
+
+$block->addFormField(
+  $factory->getTextField("locale", $User['localePreference'], ""),
+  $factory->getLabel("locale")
+);
 
 $block->addButton($factory->getSaveButton($page->getSubmitAction()));
 
@@ -78,24 +92,43 @@ $serverScriptHelper->destructor();
 <?php print($block->toHtml()); ?>
 
 <?php print($page->toFooterHtml());
+
+
 /*
-Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+Copyright (c) 2003 Sun Microsystems, Inc. 
+All Rights Reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright 
+   notice, this list of conditions and the following disclaimer.
 
--Redistribution of source code must retain the above copyright notice, this  list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in 
+   the documentation and/or other materials provided with the 
+   distribution.
 
--Redistribution in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation and/or 
-other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its 
+   contributors may be used to endorse or promote products derived 
+   from this software without specific prior written permission.
 
-Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-be used to endorse or promote products derived from this software without 
-specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
 
-This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+You acknowledge that this software is not designed or intended for 
+use in the design, construction, operation or maintenance of any 
+nuclear facility.
 
-You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
 */
+
 ?>
