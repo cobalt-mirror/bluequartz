1d5f4cbf22e2dcab87d2b6cab5f4d8ef8cea28e2
Michael Stauber <mstauber@blueonyx.it>
2014-04-04 20:08:51 +0000

    0.2-1BX08 - Modified web/login.php to use libPhp/BXBrowserLocale.php to properly   detect the browser locale.

--- BlueOnyx/5106R/alpine.mod/web/login.php
+++ BlueOnyx/5106R/alpine.mod/web/login.php
@@ -1,7 +1,6 @@
 <?php
-// Author: Mike Waychison, Kevin K.M. Chiu
-// Copyright 2000, 2001 Sun Microsystems, Inc.  All rights reserved.
-// $Id: login.php,v 1.3 2001/10/29 09:03:18 pbose Exp $
+// Author: Mike Waychison, Kevin K.M. Chiu, Michael Stauber
+// $Id: login.php
 
 // Prevent cross site scripting attacks (suggested by Ken Marcus):
 if (preg_match('/iframe/', $target)) {
@@ -13,17 +12,17 @@ if (preg_match('/alert/', $target)) {
 
 include_once("I18n.php");
 include_once("uifc/Stylist.php");
+include_once("BXBrowserLocale.php");
+
+// Detect the browser locale to see if it is supported.
+// If not, fall back to 'en_US':
+$locale = BXBrowserLocale::prefered_language();
 
 // check to accept license?
 include_once("CceClient.php");
 $cceClient = new CceClient();
 if(!$cceClient->connect()) {
-  $locale = $HTTP_ACCEPT_LANGUAGE;
-  if($HTTP_ACCEPT_LANGUAGE == "") {
-    include_once("System.php");
-    $system = new System();
-    $locale = $system->getConfig("defaultLocale");
-  }
+
   $i18n = new I18n("palette", $locale);
 
   $cceDown = "<div style=\"text-align: center;\"><br><br><br><br><span style=\"color: #990000;\">"
@@ -40,12 +39,6 @@ if ( ! $system['isLicenseAccepted'] ) {
 }
 
 // determine the local to use..
-$locale = $HTTP_ACCEPT_LANGUAGE;
-if ($HTTP_ACCEPT_LANGUAGE == "") {
-	include_once("System.php");
-	$system = new System();
-	$locale = $system->getConfig("defaultLocale");
-}
 $i18n = new I18n("base-alpine", $locale);
 
 $stylist = new Stylist();
@@ -217,24 +210,39 @@ if(cssEnabled && cookieEnabled){
 </html>
 
 <!--
-Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
-
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
--Redistribution of source code must retain the above copyright notice, this  list of conditions and the following disclaimer.
-
--Redistribution in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation and/or 
-other materials provided with the distribution.
-
-Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-be used to endorse or promote products derived from this software without 
-specific prior written permission.
-
-This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-
-You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# 
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# Copyright (c) 2003 Sun Microsystems, Inc. 
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#   notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#   notice, this list of conditions and the following disclaimer in 
+#   the documentation and/or other materials provided with the 
+#   distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#   contributors may be used to endorse or promote products derived 
+#   from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
 -->
-
-
