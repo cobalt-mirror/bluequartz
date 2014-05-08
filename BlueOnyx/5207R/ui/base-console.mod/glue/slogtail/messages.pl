892f2b4a91ada33b8a2bb525f66502fe31bb8251
Michael Stauber <mstauber@blueonyx.it>
2014-05-08 02:26:56 +0000

    2.0.0-0BX01 - Major version number bump for 520XR. - Added requirement for perl-File-ReadBackwards - Modified glue/slogtail/messages.pl to use perl-File-ReadBackwards so   that we can exclude the new SSH related CODB transactions from being   shown in the GUIs logfile viewer.

--- BlueOnyx/5207R/ui/base-console.mod/glue/slogtail/messages.pl
+++ BlueOnyx/5207R/ui/base-console.mod/glue/slogtail/messages.pl
@@ -2,5 +2,20 @@
 
 $ENV{'PATH'} = '/usr/bin';
 
-system("/usr/bin/tail -200 /var/log/messages");
+use File::ReadBackwards;
 
+$log = "/var/log/messages";
+
+$bw = File::ReadBackwards->new($log) or die "can't read $log $!" ;
+
+$num = '0';
+while( defined( $log_line = $bw->readline ) ) {
+		# We sure do NOT want the SSH related stuff visible in the GUI:
+		unless (($log_line =~ /(.*)SSHauthKeys(.*)/) || ($log_line =~ /(.*)\. SSH (.*)/)) {
+        	print $log_line ;
+        	$num++;
+        	if ($num >= '200') {
+        		exit;
+        	}
+        }
+}
