dd6b85b838c643b05d055cb7491d3cacc956b56b
Michael Stauber <mstauber@blueonyx.it>
2014-05-17 04:22:31 +0000

    .99.1-1BX08 - Small modification to ccewrap/cced_unstuck.sh as the killall command   doesn't do well with Zombies. Now uses a grepped ps for cced as well.

--- BlueOnyx/5207R/utils/cce/ccewrap/cced_unstuck.sh
+++ BlueOnyx/5207R/utils/cce/ccewrap/cced_unstuck.sh
@@ -2,6 +2,7 @@
 /usr/bin/killall -9 /usr/sausalito/sbin/cced
 /usr/bin/killall -9 pperld
 /usr/bin/killall -9 cced.init
+ps aux | grep cced | grep -v kill | awk '{print $2}' | xargs kill -9 
 /sbin/service cced.init rehash
 echo "OK"
 exit 0
