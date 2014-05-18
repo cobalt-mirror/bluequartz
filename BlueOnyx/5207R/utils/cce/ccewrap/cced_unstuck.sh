c3b2d780cbb523b7bcf28174ddf374a1db347727
Michael Stauber <mstauber@blueonyx.it>
2014-05-18 22:44:15 +0000

    0.99.1-1BX09 - Added a bit more professionalism to ccewrap/cced_unstuck.sh - Added /usr/bin/sar to ccewrap/ccewrap.conf for stats galore.

--- BlueOnyx/5207R/utils/cce/ccewrap/cced_unstuck.sh
+++ BlueOnyx/5207R/utils/cce/ccewrap/cced_unstuck.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-/usr/bin/killall -9 /usr/sausalito/sbin/cced
-/usr/bin/killall -9 pperld
-/usr/bin/killall -9 cced.init
-ps aux | grep cced | grep -v kill | awk '{print $2}' | xargs kill -9 
-/sbin/service cced.init rehash
-echo "OK"
+# This shell script is used to kill off and restart
+# a stuck CCEd instance fast and more or less relieably.
+# At least as best is possible.
+/usr/bin/killall -9 /usr/sausalito/sbin/cced >/dev/null 2>&1
+/usr/bin/killall -9 pperld >/dev/null 2>&1
+/usr/bin/killall -9 cced.init >/dev/null 2>&1
+/bin/ps aux | /bin/grep cced | /bin/grep -v cced_unstuck | /bin/grep -v kill | /bin/awk '{print $2}' | /usr/bin/xargs -I {} kill -9 {} >/dev/null 2>&1
+/sbin/service cced.init rehash >/dev/null 2>&1
 exit 0
