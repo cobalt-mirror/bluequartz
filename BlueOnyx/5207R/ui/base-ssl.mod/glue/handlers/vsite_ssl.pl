5bb3603848b965074022a34f3b1bacbe9353e19a
Michael Stauber <mstauber@blueonyx.it>
2014-07-08 18:49:55 +0000

    1.2.0-0BX04 - Modified glue/handlers/vsite_ssl.pl to allow multiple SSL enabled Vsites   on the same IP-Address.

--- BlueOnyx/5207R/ui/base-ssl.mod/glue/handlers/vsite_ssl.pl
+++ BlueOnyx/5207R/ui/base-ssl.mod/glue/handlers/vsite_ssl.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -I/usr/sausalito/perl -I/usr/sausalito/handlers/base/ssl
 # $Id: vsite_ssl.pl
-# Copyright 2000, 2001 Sun Microsystems, Inc., All rights reserved.
 # update siteAdminCaps property when enabling/disabling ssl
 
 my $DEBUG = 0;
@@ -18,51 +17,65 @@ my $oid = $cce->event_oid();
 my ($ok, $ssl_info) = $cce->get($oid, 'SSL');
 $DEBUG && print STDERR Dumper($vsite, $ssl_info);
 
-# make sure two sites with the same IP address don't both get SSL enabled
-my @other_sites = $cce->findx('Vsite', 
-                        { 'ipaddr' => $vsite->{ipaddr}, 'SSL.enabled' => 1 });
-
-if ($ssl_info->{enabled} && scalar(@other_sites) > 1)
-{
-    my $oid_to_get = $other_sites[0] != $oid ? $other_sites[0] : $other_sites[1];
-    ($ok, my $other_site) = $cce->get($oid_to_get);
-                                
-    $cce->bye('FAIL', 'sslAlreadyUsedByIP', 
-                { 
-                    'fqdn' => $other_site->{fqdn}, 
-                    'ipaddr' => $other_site->{ipaddr} 
-                });
-    exit(1);
-}
+## make sure two sites with the same IP address don't both get SSL enabled
+#my @other_sites = $cce->findx('Vsite', { 'ipaddr' => $vsite->{ipaddr}, 'SSL.enabled' => 1 });
+#
+#if ($ssl_info->{enabled} && scalar(@other_sites) > 1) {
+#    my $oid_to_get = $other_sites[0] != $oid ? $other_sites[0] : $other_sites[1];
+#    ($ok, my $other_site) = $cce->get($oid_to_get);
+#                                
+#    $cce->bye('FAIL', 'sslAlreadyUsedByIP', 
+#                { 
+#                    'fqdn' => $other_site->{fqdn}, 
+#                    'ipaddr' => $other_site->{ipaddr} 
+#                });
+#    exit(1);
+#}
 
 # set VirtualHost.ssl 
 my ($void) = $cce->find('VirtualHost', {'name' => $vsite->{name}}); 
 $cce->set($void, '', {'ssl' => $ssl_info->{enabled}}); 
 
 # update siteAdminCaps in Vsite
-if (!$cce->event_is_destroy())
-{
+if (!$cce->event_is_destroy()) {
     vsite_update_site_admin_caps($cce, $vsite, 'siteSSL', $ssl_info->{enabled});
 }
 
 $cce->bye('SUCCESS');
 exit(0);
-# Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+
 # 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# Copyright (c) 2003 Sun Microsystems, Inc. 
+# All Rights Reserved.
 # 
-# -Redistribution of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
+# 1. Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
 # 
-# -Redistribution in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation  
-# and/or other materials provided with the distribution.
+# 2. Redistributions in binary form must reproduce the above copyright 
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the 
+#     distribution.
 # 
-# Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-# be used to endorse or promote products derived from this software without 
-# specific prior written permission.
+# 3. Neither the name of the copyright holder nor the names of its 
+#     contributors may be used to endorse or promote products derived 
+#     from this software without specific prior written permission.
 # 
-# This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 # 
-# You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
