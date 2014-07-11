f14981bcc1e3c2b870976352f8b0f270565f4386
Michael Stauber <mstauber@blueonyx.it>
2014-07-11 21:34:31 +0000

    2.1.0-0BX08 - Chorizo GUI added, old GUI disabled. - Added improved translation as provided by Meaulnes Legler.

---
+++ BlueOnyx/5207R/ui/base-mysql.mod/ui/chorizo/web/config/routes.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$route['mysql/mysqlserver/:any'] = "mysqlserver";
+$route['mysql/mysqlconfig'] = "mysqlconfig";
+$route['mysql/mysql_amdetails'] = "mysql_amdetails";
+
