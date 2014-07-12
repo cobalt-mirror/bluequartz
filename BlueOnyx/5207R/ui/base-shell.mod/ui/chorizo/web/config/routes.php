f9f114bf9b8b6212c98fd2e95d4872620db51a90
Michael Stauber <mstauber@blueonyx.it>
2014-07-12 01:50:54 +0000

    1.1-0BX03 - Chorizo GUI added, old GUI disabled. - Added improved French translation as provided my Meaulnes Legler.

---
+++ BlueOnyx/5207R/ui/base-shell.mod/ui/chorizo/web/config/routes.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$route['shell/shellconfig'] = "shellconfig";
+$route['shell/vsiteShell'] = "vsiteShell";
+$route['shell/personalSSH'] = "personalSSH";
