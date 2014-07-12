9de3c901748c8cea9af00cc821c50c96ccf1a36e
Michael Stauber <mstauber@blueonyx.it>
2014-07-12 17:20:30 +0000

    1.2.0-0BX06 - Chorizo GUI added, old GUI disabled. - Added improved French translation as provided my Meaulnes Legler. - Various cosmetic fixes.

--- BlueOnyx/5207R/ui/base-system.mod/glue/handlers/etcresolv.pl
+++ BlueOnyx/5207R/ui/base-system.mod/glue/handlers/etcresolv.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w -I/usr/sausalito/perl -I.
-# $Id: etcresolv.pl,v 1.4 2001/07/14 05:38:42 mpashniak Exp $
-# Copyright 2000, 2001 Sun Microsystems, Inc., All rights reserved.
+# $Id: etcresolv.pl
 # updates /etc/resolve
 #
 # depends on:
@@ -67,22 +66,38 @@ Sauce::Util::chmodfile(0644, $fileName);
 $cce->bye('SUCCESS');
 exit(0);
 
-# Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
 # 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# Copyright (c) 2003 Sun Microsystems, Inc. 
+# All Rights Reserved.
 # 
-# -Redistribution of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
+# 1. Redistributions of source code must retain the above copyright 
+#  notice, this list of conditions and the following disclaimer.
 # 
-# -Redistribution in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation  
-# and/or other materials provided with the distribution.
+# 2. Redistributions in binary form must reproduce the above copyright 
+#  notice, this list of conditions and the following disclaimer in 
+#  the documentation and/or other materials provided with the 
+#  distribution.
 # 
-# Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-# be used to endorse or promote products derived from this software without 
-# specific prior written permission.
+# 3. Neither the name of the copyright holder nor the names of its 
+#  contributors may be used to endorse or promote products derived 
+#  from this software without specific prior written permission.
 # 
-# This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 # 
-# You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
---
+++ BlueOnyx/5207R/ui/base-system.mod/ui/chorizo/web/controllers/sysinfo.php
@@ -0,0 +1,210 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Sysinfo extends MX_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Past the login page this loads the page for /system/sysinfo.
+	 *
+	 */
+
+	public function index() {
+
+		$CI =& get_instance();
+
+	    // We load the BlueOnyx helper library first of all, as we heavily depend on it:
+	    $this->load->helper('blueonyx');
+	    init_libraries();
+
+  		// Need to load 'BxPage' for page rendering:
+  		$this->load->library('BxPage');
+		$MX =& get_instance();
+
+	    // Get $sessionId and $loginName from Cookie (if they are set):
+	    $sessionId = $CI->input->cookie('sessionId');
+	    $loginName = $CI->input->cookie('loginName');
+	    $locale = $CI->input->cookie('locale');
+
+	    // Line up the ducks for CCE-Connection:
+	    include_once('ServerScriptHelper.php');
+		$serverScriptHelper = new ServerScriptHelper($sessionId, $loginName);
+		$cceClient = $serverScriptHelper->getCceClient();
+		$user = $cceClient->getObject("User", array("name" => $loginName));
+		$i18n = new I18n("base-system", $user['localePreference']);
+		$system = $cceClient->getObject("System");
+
+		// Initialize Capabilities so that we can poll the access rights as well:
+		$Capabilities = new Capabilities($cceClient, $loginName, $sessionId);
+
+		// -- Actual page logic start:
+
+		// Not 'serverInformation'? Bye, bye!
+		if (!$Capabilities->getAllowed('serverInformation')) {
+			// Nice people say goodbye, or CCEd waits forever:
+			$cceClient->bye();
+			$serverScriptHelper->destructor();
+			Log403Error("/gui/Forbidden403");
+		}
+
+		//
+		//--- Get CODB-Objects of interest: 
+		//
+
+		// refresh information
+		$unique = microtime();
+		$cceClient->setObject("System", array("refresh" => $unique), "Memory");
+
+		$product = new Product($cceClient);
+
+		if(!$product->isRaq()) {
+		  $cceClient->setObject("System", array("refresh" => $unique), "Disk");
+		}
+
+		// get objects
+		$system = $cceClient->getObject("System", array(), "");
+		$systemDisk = $cceClient->getObject("System", array(), "Disk");
+		$systemMemory = $cceClient->getObject("System", array(), "Memory");
+		$eth0 = $cceClient->getObject("Network", array("device" => "eth0"));
+		$eth1 = $cceClient->getObject("Network", array("device" => "eth1"));
+
+		//
+	    //-- Generate page:
+	    //
+
+		// Prepare Page:
+		$factory = $serverScriptHelper->getHtmlComponentFactory("base-system", "/system/sysinfo");
+		$BxPage = $factory->getPage();
+		$BxPage->setErrors(array());
+		$i18n = $factory->getI18n();
+
+		// Set Menu items:
+		$BxPage->setVerticalMenu('base_serverconfig');
+		$page_module = 'base_sysmanage';
+
+		$defaultPage = "basicSettingsTab";
+
+		$block =& $factory->getPagedBlock("systemInformation", array($defaultPage));
+
+		$block->setToggle("#");
+		$block->setSideTabs(FALSE);
+		$block->setDefaultPage($defaultPage);
+
+		if($system["productName"] != "") {
+		  $block->addFormField(
+		    $factory->getTextField("productNameField", $system["productName"], "r"),
+		    $factory->getLabel("productNameField")
+		  );
+		}
+
+		if ($system['productBuild']) {
+		  $block->addFormField(
+		    $factory->getTextField("productBuildField", $system["productBuild"], "r"),
+		    $factory->getLabel("productBuildField")
+		  );
+		}
+
+		// System may contain the literal "Uninitialized"
+		$formattedSerial = $system["productSerialNumber"];
+		if($formattedSerial != "") {
+		  if($formattedSerial == 'Uninitialized') {
+		    $formattedSerial = $i18n->get("serialUninitialized");
+		  }
+
+		  $block->addFormField(
+		    $factory->getTextField("productSerialNumberField",
+		      $formattedSerial, "r"),
+		    $factory->getLabel("productSerialNumberField")
+		  );
+		}
+
+		if($system["serialNumber"] != "") {
+		  $block->addFormField(
+		    $factory->getTextField("serialNumberField", $system["serialNumber"], "r"),
+		    $factory->getLabel("serialNumberField")
+		  );
+		}
+
+		if($eth0["mac"] != "") {
+		  $block->addFormField(
+		    $factory->getMacAddress("mac0Field", $eth0["mac"], "r"),
+		    $factory->getLabel("mac0Field")
+		  );
+		}
+
+		if($eth1["mac"] != "") {
+		  $block->addFormField(
+		    $factory->getMacAddress("mac1Field", $eth1["mac"], "r"),
+		    $factory->getLabel("mac1Field")
+		  );
+		}
+
+		// convert to GB
+		$diskTotal = round($systemDisk["disk1Total"]*10/1024/1024)/10;
+		if($diskTotal != 0) {
+		  $block->addFormField(
+		    $factory->getInteger("diskField", $diskTotal, "", "", "r"),
+		    $factory->getLabel("diskField")
+		  );
+		}
+
+		if($systemMemory["physicalMemTotal"] != "") {
+		  $block->addFormField(
+		    $factory->getInteger("memoryField", $systemMemory["physicalMemTotal"], "", "", "r"),
+		    $factory->getLabel("memoryField")
+		  );
+		}
+
+		// Add Button-Link for the www.BlueOnyx.it website:
+		$webLink = $factory->getButton($i18n->get("webLink"), "webLinkText");
+		$webLink->setTarget("_blank");
+		$buttonContainer = $factory->getButtonContainer("", array($webLink));
+
+		// Nice people say goodbye, or CCEd waits forever:
+		$cceClient->bye();
+		$serverScriptHelper->destructor();
+
+		$page_body[] = $block->toHtml();
+		$page_body[] = $buttonContainer->toHtml();
+
+		// Out with the page:
+	    $BxPage->render($page_module, $page_body);
+
+	}		
+}
+/*
+Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+All Rights Reserved.
+
+1. Redistributions of source code must retain the above copyright 
+   notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in 
+   the documentation and/or other materials provided with the 
+   distribution.
+
+3. Neither the name of the copyright holder nor the names of its 
+   contributors may be used to endorse or promote products derived 
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+You acknowledge that this software is not designed or intended for 
+use in the design, construction, operation or maintenance of any 
+nuclear facility.
+
+*/
+?>
