e688d47923d8e518ce241dbe5b24299ca3668789
Michael Stauber <mstauber@blueonyx.it>
2014-07-13 23:43:01 +0000

    Move of ui/palette to platform/palette, as it now contains 520XR only code that is no longer backwards compatible.

--- BlueOnyx/5207R/ui/palette/web/libJs/uifc/ScrollList.js
+++
@@ -1,9 +0,0 @@
-function ScrollList_selectAllSwitch(element) {
-  var form = element.form;
-  var entryIdsString = form.elements._entryIds.value;
-  var entryIds = entryIdsString.split(',');
-  for (var i = 0; i<entryIds.length; i++) {
-    if (form[entryIds[i]] != null) 
-      form[entryIds[i]].checked = element.checked;
-  }
-}
