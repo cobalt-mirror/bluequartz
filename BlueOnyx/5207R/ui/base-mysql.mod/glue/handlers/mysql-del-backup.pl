d2e8a95c4435aecf6fc869ebe9409c35c4e79d45
Michael Stauber <mstauber@blueonyx.it>
2014-07-16 23:00:31 +0000

    2.1.0-0BX13 - Fixed path in glue/handlers/mysql-del-backup.pl as well.

--- BlueOnyx/5207R/ui/base-mysql.mod/glue/handlers/mysql-del-backup.pl
+++ BlueOnyx/5207R/ui/base-mysql.mod/glue/handlers/mysql-del-backup.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -I/usr/sausalito/perl
 # Author: Brian N. Smith
-# Copyright 2006, NuOnce Networks, Inc. All rights reserved.
-# $Id: mysql-del-backup.pl,v 1.0 2006/08/09 10:22:00 Exp $
+# $Id: mysql-del-backup.pl
 
 use CCE;
 
@@ -16,7 +15,43 @@ if (!defined($oids[0])) {
         exit 0;
 }
 
-unlink "/usr/sausalito/ui/web/base/mysql/mysql-dump.sql";
+unlink "/usr/sausalito/ui/chorizo/ci/application/modules/base/mysql/models/mysql-dump.sql";
 
 $cce->bye('SUCCESS');
 exit 0;
+
+# 
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# Copyright (C) 2006, NuOnce Networks, Inc. All rights reserved.
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#	 notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#	 notice, this list of conditions and the following disclaimer in 
+#	 the documentation and/or other materials provided with the 
+#	 distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#	 contributors may be used to endorse or promote products derived 
+#	 from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
