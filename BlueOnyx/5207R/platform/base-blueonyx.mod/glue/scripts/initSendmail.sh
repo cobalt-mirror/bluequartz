c587d2c50d5d0ad3143a72b852eaca2aaabf7ed2
Michael Stauber <mstauber@blueonyx.it>
2014-09-10 00:30:56 +0000

    4.20140909-BX03 - Modified glue/scripts/initSendmail.sh to remove the symlink for /etc/aliases - Also changed the makemap hash to a proper newaliases run instead. - Added constructor alias_fix.pl.

--- BlueOnyx/5207R/platform/base-blueonyx.mod/glue/scripts/initSendmail.sh
+++ BlueOnyx/5207R/platform/base-blueonyx.mod/glue/scripts/initSendmail.sh
@@ -4,14 +4,12 @@ CONFDIR='/usr/sausalito/configs/sendmail'
 if [ ! -f /etc/mail/aliases ]; then
   if [ -f /etc/mail/aliases.rpmsave ]; then
     mv /etc/mail/aliases.rpmsave /etc/mail/aliases
-  else
-    ln -s /etc/aliases /etc/mail/aliases
   fi
   grep '^root:' /etc/mail/aliases > /dev/null 2>&1
   if [ $? = 1 ]; then
     echo 'root:		admin' >> /etc/mail/aliases
   fi
-  /usr/sbin/makemap hash /etc/mail/aliases.db < /etc/mail/aliases
+  /usr/bin/newaliases
 fi
 
 # Handle Mailman presence:
