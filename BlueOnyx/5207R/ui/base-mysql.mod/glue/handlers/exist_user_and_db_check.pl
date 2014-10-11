050280f9237d20463425e695277b147b6c598596
Michael Stauber <mstauber@blueonyx.it>
2014-10-11 23:21:41 +0000

    2.2.0-0BX01 - Version number bump due to radical behaviour changes. - Implemented, improvised and adapted Hideki Oride's MySQL changes from   the BlueQuartz 5200R code tree. It's indeed way better to offload the   creation/deletion of MySQL users and databases into Handlers instead   of stuffing that into the UI or into UIExtensions. That way we can now   also easily check if a database or DB user already exists, whereas   that would have complicated the MySQL UIExtensions just a bit more.   Database usernames and Database names are now auto-generated and   prefixed with 'vsite_', which allows us to also already fill in the   username and database name on Vsite creation, as we no longer would   need to predict the Vsite group. This also sorts the follow on   problem that after a cmuImport site2 might have the database user   'site44' and the database name 'site44_db'. We no longer use the   group for that, but instead a randomized 7 character alpha-numeric   string. - Modified glue/conf/mysql.conf - Added glue/handlers/destroy_mysql_data.pl - Added glue/handlers/create_mysql_data.pl - Modified and activated glue/handlers/exist_user_and_db_check.pl - Renamed most schema files for easier comprehension. - Cleanup of ui/chorizo/extensions/15_MYSQL_Vsite.php.create.Vsite - Cleanup of ui/chorizo/extensions/15_MYSQL_Vsite.php.modifyWeb.Vsite - Grammar fix in locale/de_DE/mysql.po

--- BlueOnyx/5207R/ui/base-mysql.mod/glue/handlers/exist_user_and_db_check.pl
+++ BlueOnyx/5207R/ui/base-mysql.mod/glue/handlers/exist_user_and_db_check.pl
@@ -1,23 +1,21 @@
 #!/usr/bin/perl -I/usr/sausalito/perl
 #
 # exist_user_and_db_check.pl
-# Hideki Oride <oride@gachapom.jp>
 
 use CCE;
 use POSIX qw(isalpha);
 use DBI;
 
-my $DEBUG = 0;
-$DEBUG && open(STDERR, ">>/tmp/validate-mysql");
-$DEBUG && warn $0.' '.`date`;
+# Debugging switch:
+$DEBUG = "0";
+if ($DEBUG)
+{
+		use Sys::Syslog qw( :DEFAULT setlogsock);
+}
 
 $cce = new CCE;
 
 $cce->connectfd();
-
-# Nice idea, but this needs more work. So we just die here for now:
-$cce->bye('SUCCESS');
-exit(0);
 
 my $vsite     = $cce->event_object();
 my $vsite_oid = $cce->event_oid();
@@ -26,118 +24,136 @@ my $vsite_old = $cce->event_old();
 
 # Get System Information
 my @oids = $cce->find("System");
-$DEBUG && print STDERR Dumper(@oids);
 my ($ok, $system) = $cce->get($oids[0]);
 
 # Get MySQL Information
 my @oids = $cce->find("MySQL");
-$DEBUG && print STDERR Dumper(@oids);
 my ($ok, $mysql) = $cce->get($oids[0]);
 
 # Get 'MYSQL_Vsite' details:
 ($ok, $vsite_mysql) = $cce->get($vsite_oid, 'MYSQL_Vsite');
-$DEBUG && print STDERR Dumper($system, $mysql, $vsite, $vsite_mysql);
-$DEBUG && warn "DB: $vsite_mysql->{DB}  \n";
+
+&debug_msg("DB: $vsite_mysql->{DB} \n");
 
 # Enable and Disable Check
-if($vsite_mysql->{enabled} != 1)
-{
-    $cce->bye('SUCCESS');
-    exit(0);
+if ($vsite_mysql->{enabled} != 1) {
+	$cce->bye('SUCCESS');
+	exit(0);
 }
 
 
 ## MySQL Server Connection Check
 $dbh = DBI->connect(
-        "DBI:mysql:mysql:$mysql->{sql_host}:$mysql->{port}",
-        $mysql->{sql_root}, $mysql->{sql_rootpassword},
-        {
-            RaiseError => 0,
-            PrintError => 0
-        }
+		"DBI:mysql:mysql:$mysql->{sql_host}:$mysql->{port}",
+		$mysql->{sql_root}, $mysql->{sql_rootpassword},
+		{
+			RaiseError => 0,
+			PrintError => 0
+		}
 ); 
-$DEBUG && print STDERR Dumper($dbh);
 
-if(!$dbh)
-{
-    $message .= '[[base-mysql.amNotAccepting]]';
-    $db_check_fail = 1;
-    $cce->bye('FAIL', $message);
-    exit(1);
+if (!$dbh) {
+	$message .= '[[base-mysql.amNotAccepting]]';
+	$db_check_fail = 1;
+	&debug_msg("FAIL: $message \n");
+	$cce->bye('FAIL', $message);
+	exit(1);
 }
 
 # User Check
 my $siteMySQLUser = $dbh->quote($vsite_mysql->{username});
-$DEBUG && warn "SQL: SELECT COUNT(*) as rowCount FROM `user` WHERE User = $siteMySQLUser  \n";
+&debug_msg("SQL: SELECT COUNT(*) as rowCount FROM `user` WHERE User = $siteMySQLUser \n");
 @row = $dbh->selectrow_array("SELECT COUNT(*) as rowCount FROM `user` WHERE User = $siteMySQLUser");
-$DEBUG && warn "row[0]:$row[0]  \n";
-$DEBUG && print STDERR Dumper(@row);
-if($row[0] != 0)
-{
-    $message .= '[[base-mysql.CreateFailUserExists]]';
-    $db_check_fail = 1;
+&debug_msg("row[0]:$row[0] \n");
+
+if ($row[0] != 0) {
+	$message .= '[[base-mysql.CreateFailUserExists]]';
+	$db_check_fail = 1;
 }
 
 # DB Check
 my $siteMySQLDb = $dbh->quote($vsite_mysql->{DB});
-$DEBUG && warn "SQL: SELECT COUNT(*) as rowCount FROM `db` WHERE User = $siteMySQLUser AND Db = $siteMySQLDb \n";
+&debug_msg("SQL: SELECT COUNT(*) as rowCount FROM `db` WHERE User = $siteMySQLUser AND Db = $siteMySQLDb \n");
 @row = $dbh->selectrow_array("SELECT COUNT(*) as rowCount FROM `db` WHERE User = $siteMySQLUser AND Db = $siteMySQLDb");
-$DEBUG && warn "row[0]:$row[0]  \n";
-$DEBUG && print STDERR Dumper(@row);
-if($row[0] != 0)
-{
-    $message .= '[[base-mysql.CreateFailDBExists]]';
-    $db_check_fail = 1;
+&debug_msg("row[0]:$row[0] \n");
+
+if ($row[0] != 0) {
+	$message .= '[[base-mysql.CreateFailDBExists]]';
+	$db_check_fail = 1;
 }
 
 $dbh->disconnect;
 
 ## Database Connection Check
 $dbh = DBI->connect(
-        "DBI:mysql:$vsite_mysql->{DB}:$mysql->{sql_host}:$mysql->{port}",
-        $mysql->{sql_root}, $mysql->{sql_rootpassword},
-        {
-            RaiseError => 0,
-            PrintError => 0
-        }
-); 
-$DEBUG && print STDERR Dumper($dbh);
-if($dbh)
-{
-    # $dbh->diconnect;
-    $message .= '[[base-mysql.CreateFailDatabaseExists]]';
-    $db_check_fail = 1;
+		"DBI:mysql:$vsite_mysql->{DB}:$mysql->{sql_host}:$mysql->{port}",
+		$mysql->{sql_root}, $mysql->{sql_rootpassword},
+		{
+			RaiseError => 0,
+			PrintError => 0
+		}
+);
+&debug_msg("Dumper" . Dumper($dbh) . "\n"); 
+
+if ($dbh) {
+	# $dbh->diconnect;
+	$message .= '[[base-mysql.CreateFailDatabaseExists]]';
+	$db_check_fail = 1;
 }
 
-
-if($db_check_fail) 
-{
-    $cce->bye('FAIL', $message);
-    exit(1);
+if ($db_check_fail)  {
+	&debug_msg("FAIL: $message \n");
+	$cce->bye('FAIL', $message);
+	exit(1);
 }
 
 $cce->bye('SUCCESS');
 exit(0);
 
+sub debug_msg {
+	if ($DEBUG) {
+		my $msg = shift;
+		$user = $ENV{'USER'};
+		setlogsock('unix');
+		openlog($0,'','user');
+		syslog('info', "$ARGV[0]: $msg");
+		closelog;
+	}
+}
+
 # 
 # Copyright (c) 2010 Hideki Oride <oride@gachapom.jp>
-# Copyright (c) 2013 Team BlueOnyx, BLUEONYX.IT
-# Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# Copyright (c) 2003 Sun Microsystems, Inc. 
+# All Rights Reserved.
 # 
-# Redistribution and use in source and binary forms, with or without modification, 
-# are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
 # 
-# -Redistribution of source code must retain the above copyright notice, this  list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright 
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the 
+#     distribution.
 # 
-# -Redistribution in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation and/or 
-# other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its 
+#     contributors may be used to endorse or promote products derived 
+#     from this software without specific prior written permission.
 # 
-# Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-# be used to endorse or promote products derived from this software without 
-# specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 # 
-# This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-# 
-# You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
 # 
