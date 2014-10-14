7e17ce6ba7a802cd060a1423ed5131e1f6d4f8bb
Michael Stauber <mstauber@blueonyx.it>
2014-10-14 01:30:52 +0000

    1.2.0-0BX02 - Modified src/base-disk-am/get_quotas.pl to add debugging. Also added a check that if   there is only one disk (think OpenVZ, Cloud or similar) which is mounted, is 'internal'   and has the flag 'isHomePartition', then we use it, but check for the sites on /home   instead. This eleminates the last vestiges of BlueOnyx really needing a /home   partition. This is the one work around that eleminates the need for all the other   duct-tape measures in this regards. Still works if the other usual band-aids are   already applied on a box.

--- BlueOnyx/ui/base-disk.mod/src/base-disk-am/get_quotas.pl
+++ BlueOnyx/ui/base-disk.mod/src/base-disk-am/get_quotas.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/perl
-# get_quotas.pl, v 1.1.0-15BQ15 Thu Nov 27 12:33:05 2008 mstauber Exp $
-# Copyright (c) 2003 Sun Microsystems, Inc. All Rights Reserved.
-# Copyright (c) 2008 Solarspeed Ltd, All Rights Reserved.
+# get_quotas.pl
 
 use lib qw(/usr/sausalito/perl);
 use Base::HomeDir qw(homedir_get_group_dir);
 use Data::Dumper;
 use Getopt::Long;
-use strict;
+#use strict;
 use CCE;
 use Quota;
 use Unix::PasswdFile;
+
+# Debugging switch:
+$DEBUG = "0";
+if ($DEBUG)
+{
+        use Sys::Syslog qw( :DEFAULT setlogsock);
+}
 
 my $site = '';
 my $sort = '';
@@ -20,10 +25,14 @@ my $users_only = 1;
 my $sites_only = '';
 my $pw = new Unix::PasswdFile "/etc/passwd";
 
-GetOptions('site=s' => \$site, 'sort=s' => \$sort, 
-	   'descending' => \$descending, 'ascending' => sub { $descending = 0 },
-	   'help' => \$help, 'users' => sub {$users_only = 1; $sites_only = 0}, 
-	   'sites' => sub { $sites_only = 1; $users_only = 0 });
+GetOptions( 'site=s' => \$site, 
+            'sort=s' => \$sort, 
+            'descending' => \$descending, 
+            'ascending' => sub { $descending = 0 },
+            'help' => \$help, 
+            'users' => sub {$users_only = 1; $sites_only = 0}, 
+            'sites' => sub { $sites_only = 1; $users_only = 0 }
+            );
 
 if ($help) {
     print "Usage: get_quotas.pl [ --users ] [ --sites ] \n\t\t\t[ --sort=type ] [ --site=name ] [ --descending ] [ --ascending] [ --help ]\n";
@@ -62,8 +71,8 @@ if ($sort eq "usage") {
     @results = sort { @{$a}[1] <=> @{$b}[1] } @results;
 } elsif ($sort eq "quota") {
     @results = sort { if (@{$a}[2] == 0) { return 1; } # no quota = unlimited
-		    elsif (@{$b}[2] == 0) { return -1; } 
-		    else { return (@{$a}[2] <=> @{$b}[2]) } } @results;
+    elsif (@{$b}[2] == 0) { return -1; } 
+    else { return (@{$a}[2] <=> @{$b}[2]) } } @results;
 } else {
     # sort == name
     @results = sort { @{$a}[0] cmp @{$b}[0] } @results;
@@ -90,8 +99,8 @@ sub site_users {
     $cce->connectuds();
     my ($oid) = $cce->find('Vsite', { 'name' => $site });
     if (!$oid) {
-	print STDERR "couldn't find site $site in CCE\n";
-	return;
+        print STDERR "couldn't find site $site in CCE\n";
+        return;
     }
     ($site_ok, $site) = $cce->get($oid);
 
@@ -113,17 +122,17 @@ sub all_users {
     my ($name, $null, $all_gid, $user_gid, $dir);
 
     # all CCE users are NO LONGER in the "users" group - so we have do do it differently:
-	my @all_users = ();
+    my @all_users = ();
 
     foreach $name ($pw->users) {
         my $uid = $pw->uid($name);
         my $user_gid = $pw->gid($name);
         my $dir = $pw->home($name);
-	my @groupworkaround = split(/\//, $dir);
+        my @groupworkaround = split(/\//, $dir);
 
         if ((($uid >= 500)&&($uid != 65534)) && ($groupworkaround[5] ne "logs")) {
-    	    push @all_users, $name;
-	}
+            push @all_users, $name;
+        }
     }
     return @all_users;
 }
@@ -134,32 +143,41 @@ sub sites {
     # find all disks
     my $cce = new CCE;
     $cce->connectuds();
+
+    my (@AllDisks) = $cce->find('Disk', { 'internal' => 1 });
+    my $num_of_disks = @AllDisks;
+    &debug_msg("Found " . $num_of_disks . " internal disk(s). \n");
+
     my (@disks) = $cce->find('Disk', { 'isHomePartition' => 1 });
-    
+
     # find all mountpoints
     my @mounts = ();
     foreach my $disk (@disks) {
-	my ($ok, $obj) = $cce->get($disk);
-	push @mounts, $obj->{mountPoint};
+        my ($ok, $obj) = $cce->get($disk);
+        push @mounts, $obj->{mountPoint};
     }
 
     # find all numeric hashes
     my @hashdirs = ();
     foreach my $mount (@mounts) {
-	opendir(SITEDIR, "$mount/.sites");
-	my @dirs = map { "$mount/.sites/$_" } grep /^\d+$/, readdir(SITEDIR);
-	push @hashdirs, @dirs;
-	close(SITEDIR);
+        &debug_msg("Processing disk for mountpoint " . $mount . "\n");
+        if (($num_of_disks == "1") && ($mount eq "/")) {
+            $mount = '/home';
+            &debug_msg("As this is the only disk, we look for the Vsites on " . $mount . " instead.\n");
+        }
+        opendir(SITEDIR, "$mount/.sites");
+        my @dirs = map { "$mount/.sites/$_" } grep /^\d+$/, readdir(SITEDIR);
+        push @hashdirs, @dirs;
+        close(SITEDIR);
     }
 
     # find all dirs in all hashes
     my @some_sites = ();
     foreach my $hash (@hashdirs) {
-	opendir(HASH, $hash);
-	@some_sites = grep !/^\./, readdir(HASH);
-	close(HASH);
-
-	push @sites, @some_sites;
+        opendir(HASH, $hash);
+        @some_sites = grep !/^\./, readdir(HASH);
+        close(HASH);
+        push @sites, @some_sites;
     }
 
     $cce->bye();
@@ -175,13 +193,18 @@ sub siteusage {
     # find all disks
     my $cce = new CCE;
     $cce->connectuds();
+
+    my (@AllDisks) = $cce->find('Disk', { 'internal' => 1 });
+    my $num_of_disks = @AllDisks;
+    &debug_msg("Found " . $num_of_disks . " internal disk(s). \n");
+
     my (@disks) = $cce->find('Disk', { 'isHomePartition' => 1 });
 
     # find all mountpoints
     my @mounts = ();
     foreach my $disk (@disks) {
-	my ($ok, $obj) = $cce->get($disk);
-	push @mounts, $obj->{mountPoint};
+        my ($ok, $obj) = $cce->get($disk);
+        push @mounts, $obj->{mountPoint};
     }
 
     # this relies on Alpine's hashing scheme. if the hashing scheme changes
@@ -189,34 +212,40 @@ sub siteusage {
     # find all numeric hashes
     my @hashdirs = ();
     foreach my $mount (@mounts) {
-	opendir(SITEDIR, "$mount/.sites");
-	my @dirs = map { "$mount/.sites/$_" } grep /^\d+$/, readdir(SITEDIR);
-	push @hashdirs, @dirs;
-	close(SITEDIR);
+        &debug_msg("Processing disk for mountpoint " . $mount . "\n");
+        if (($num_of_disks == "1") && ($mount eq "/")) {
+            $mount = '/home';
+            &debug_msg("As this is the only disk, we look for the Vsites on " . $mount . " instead.\n");
+        }
+        opendir(SITEDIR, "$mount/.sites");
+        my @dirs = map { "$mount/.sites/$_" } grep /^\d+$/, readdir(SITEDIR);
+        push @hashdirs, @dirs;
+        close(SITEDIR);
     }
 
     # find all dirs in all hashes
     my @some_sites = ();
     foreach my $hash (@hashdirs) {
-	opendir(HASH, $hash);
-	@some_sites = grep !/^\./, readdir(HASH);
-	close(HASH);
+        opendir(HASH, $hash);
+        &debug_msg("Processing Hash for " . $hash . "\n");
+        @some_sites = grep !/^\./, readdir(HASH);
+        close(HASH);
 
-	# lookup dev
-	my $dev = Quota::getqcarg($hash);
-	my $is_group = 1;
+        # lookup dev
+        my $dev = Quota::getqcarg($hash);
+        my $is_group = 1;
 
-	foreach my $site (@some_sites) {
-	    # lookup gid
-	    my ($name, $null, $gid) = getgrnam($site);
+        foreach my $site (@some_sites) {
+            # lookup gid
+            my ($name, $null, $gid) = getgrnam($site);
 
-	    # do query
-	    my ($used, $quota) = Quota::query($dev, $gid, $is_group);
+            # do query
+            my ($used, $quota) = Quota::query($dev, $gid, $is_group);
 
-	    $hash{$name}{used} = $used;
-	    $hash{$name}{quota} = $quota;
+            $hash{$name}{used} = $used;
+            $hash{$name}{quota} = $quota;
 
-	}
+        }
     }
 
     return %hash;
@@ -235,45 +264,72 @@ sub userusage {
         my $uid = $pw->uid($name);
         my $user_gid = $pw->gid($name);
         my $dir = $pw->home($name);
-	my @groupworkaround = split(/\//, $dir);
+        my @groupworkaround = split(/\//, $dir);
 
-	# Ignore all users with an UID below 500 and also the SITEXX-logs users:
-	if (($uid < 500)||($uid == 65534)) {
-	    next;
-	}
-	elsif ($groupworkaround[5] eq "logs") {
-	    next;
-	}
-	elsif ($name eq "nfsnobody") {
-	    next;
-	}
-	else {
-	    my $dev = Quota::getqcarg($dir);
-	    my ($used, $quota) = Quota::query($dev, $uid);
+        # Ignore all users with an UID below 500 and also the SITEXX-logs users:
+        if (($uid < 500)||($uid == 65534)) {
+            next;
+        }
+        elsif ($groupworkaround[5] eq "logs") {
+            next;
+        }
+        elsif ($name eq "nfsnobody") {
+            next;
+        }
+        else {
+            my $dev = Quota::getqcarg($dir);
+            my ($used, $quota) = Quota::query($dev, $uid);
 
-	    $hash{$name}{used} = $used;
-	    $hash{$name}{quota} = $quota;
-	}
+            $hash{$name}{used} = $used;
+            $hash{$name}{quota} = $quota;
+        }
     }
     return %hash;
 }
- 
-# Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+
+sub debug_msg {
+    if ($DEBUG) {
+        my $msg = shift;
+        $user = $ENV{'USER'};
+        setlogsock('unix');
+        openlog($0,'','user');
+        syslog('info', "$ARGV[0]: $msg");
+        closelog;
+    }
+}
+
 # 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# Copyright (c) 2003 Sun Microsystems, Inc. 
+# All Rights Reserved.
 # 
-# -Redistribution of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
+# 1. Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
 # 
-# -Redistribution in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation  
-# and/or other materials provided with the distribution.
+# 2. Redistributions in binary form must reproduce the above copyright 
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the 
+#     distribution.
 # 
-# Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-# be used to endorse or promote products derived from this software without 
-# specific prior written permission.
+# 3. Neither the name of the copyright holder nor the names of its 
+#     contributors may be used to endorse or promote products derived 
+#     from this software without specific prior written permission.
 # 
-# This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 # 
-# You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
