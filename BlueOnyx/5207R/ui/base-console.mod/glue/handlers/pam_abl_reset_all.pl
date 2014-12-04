80c1c4ebeca26aebbedc994509d4be4b348ffae6
Michael Stauber <mstauber@blueonyx.it>
2014-12-04 23:22:39 +0000

    2.0.0-0BX05 - Some Systemd love in post-install and handlers.

--- BlueOnyx/5207R/ui/base-console.mod/glue/handlers/pam_abl_reset_all.pl
+++ BlueOnyx/5207R/ui/base-console.mod/glue/handlers/pam_abl_reset_all.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl -I/usr/sausalito/perl
-# $Id: pam_abl_reset_all.pl, v1.0.0-1 Fri 07 Aug 2009 08:45:07 AM CEST mstauber Exp $
-# Copyright 2006-2009 Solarspeed Ltd. All rights reserved.
-# Copyright 2009 Team BlueOnyx. All rights reserved.
+# $Id: pam_abl_reset_all.pl
 
 # This handler is run whenever someone wants to remove all host and user blocks through the GUI
 
@@ -21,10 +19,45 @@ $oid = $cce->event_oid();
 # Make sure to only trigger on modify:
 if ($cce->event_is_modify()) {
     system('/usr/bin/pam_abl --okuser=* --okhost=*');
-    system("/etc/init.d/xinetd stop");
+    system("/sbin/service xinetd stop");
     system("/bin/rm -f /var/log/proftpd/ban.tab");
-    system("/etc/init.d/xinetd start");
+    system("/sbin/service xinetd start");
 }
 
 $cce->bye('SUCCESS');
 exit(0);
+
+# 
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#	 notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#	 notice, this list of conditions and the following disclaimer in 
+#	 the documentation and/or other materials provided with the 
+#	 distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#	 contributors may be used to endorse or promote products derived 
+#	 from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
--- BlueOnyx/5207R/ui/base-console.mod/glue/handlers/update_pam_abl_settings.pl
+++ BlueOnyx/5207R/ui/base-console.mod/glue/handlers/update_pam_abl_settings.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl -I/usr/sausalito/perl
-# $Id: update_pam_abl_settings.pl, v1.0.0-0 Wed 05 Aug 2009 05:50:53 AM CEST mstauber Exp $
-# Copyright 2006-2009 Solarspeed Ltd. All rights reserved.
-# Copyright 2009 Team BlueOnyx. All rights reserved.
+# $Id: update_pam_abl_settings.pl
 
 # This handler is run whenever pam_abl is modified through the GUI.
 
@@ -84,3 +82,37 @@ EOF
 $cce->bye('SUCCESS');
 exit(0);
 
+# 
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#    contributors may be used to endorse or promote products derived 
+#    from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
