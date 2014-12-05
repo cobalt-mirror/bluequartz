daea0ed5b0f0793c935d9cfb43a4eb7a6c0dc9c7
Michael Stauber <mstauber@blueonyx.it>
2014-12-05 02:21:35 +0000

    1.5.0-0BX04 - Systemd related fixes.

--- BlueOnyx/5207R/ui/base-swupdate.mod/glue/sbin/swupdate_tmp_clean.pl
+++ BlueOnyx/5207R/ui/base-swupdate.mod/glue/sbin/swupdate_tmp_clean.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl
-# $Id: swupdate_tmp_clean.pl Tue 17 May 2011 09:58:09 PM CEST mstauber $
-# Copyright 2011 Team BlueOnyxc. All rights reserved.
+# $Id: swupdate_tmp_clean.pl
 #
 # When PKGs are installed through the GUI, we cannot restart CCEd. Although we often
 # need to do that. Otherwise the PKG install fails if CCEd is installed while the GUI
@@ -15,8 +14,43 @@
 # then we restart CCEd and delete the file.
 
 if (-f "/tmp/.guipkginstall") {
-    system("/etc/init.d/cced.init restart > /dev/null 2>&1&");
+    system("/usr/sausalito/sbin/cced.init restart > /dev/null 2>&1&");
     system("/bin/rm -f /tmp/.guipkginstall");
 }
 
 exit(0);
+
+# 
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#	 notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#	 notice, this list of conditions and the following disclaimer in 
+#	 the documentation and/or other materials provided with the 
+#	 distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#	 contributors may be used to endorse or promote products derived 
+#	 from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
