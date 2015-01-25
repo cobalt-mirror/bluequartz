284ab5a4720586560f2b414b7d3d6a260bb1fb76
Michael Stauber <mstauber@blueonyx.it>
2015-01-25 20:59:51 +0000

    4.0-0BX15 - Modified glue/handlers/php_handler.pl to use either service or   systemctl. But systemctl must be run with --no-block for this to work.

--- BlueOnyx/5207R/ui/base-vsite.mod/glue/handlers/php_handler.pl
+++ BlueOnyx/5207R/ui/base-vsite.mod/glue/handlers/php_handler.pl
@@ -1,8 +1,6 @@
 #!/usr/bin/perl -I/usr/sausalito/perl
-# $Id: php_handler.pl, v1.2.0.0 Mon 01 Dec 2008 05:36:01 AM CET mstauber Exp $
-# Copyright 2006-2008 Solarspeed.net. All rights reserved.
-# Copyright 2008-2011 Team BlueOnyx. All rights reserved.
-
+# $Id: php_handler.pl
+#
 # This handler is run whenever a CODB Object called "PHP" is created, destroyed or 
 # modified. 
 #
@@ -201,7 +199,14 @@ sub thirdparty_check {
 
 sub restart_apache {
     # Restarts Apache - hard restart:
-    system("/sbin/service httpd restart");
+    if (-f "/usr/bin/systemctl") {
+        # Got Systemd:
+        system("systemctl restart httpd.service --no-block");
+    }
+    else {
+        # Thank God, no Systemd:
+        system("/sbin/service httpd restart");
+    }
 }
 
 sub items_of_interest {
@@ -407,3 +412,37 @@ sub debug_msg {
 $cce->bye('SUCCESS');
 exit(0);
 
+# 
+# Copyright (c) 2015 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2015 Team BlueOnyx, BLUEONYX.IT
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the 
+#     distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#     contributors may be used to endorse or promote products derived 
+#     from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
