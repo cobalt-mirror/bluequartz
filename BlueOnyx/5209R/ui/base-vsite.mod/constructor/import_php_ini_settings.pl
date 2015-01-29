1041dc5ebbdd4c6e8dc8a40dcea5ceff9666726f
Michael Stauber <mstauber@blueonyx.it>
2015-01-29 18:20:48 +0000

    4.1-0BX18 - New PHP integration for Apache 2.4, which includes PHP (DSO), mod_ruid2,   suPHP and FPM/FastCGI. This requires various changes. - Updated constructors. Mostly housekeeping stuff. - glue/schemas/reseller_caps.schema: Updated to add new Reseller   Capabilities for the new PHP options. - glue/schemas/php.schema: Housekeeping stuff. - glue/schemas/php_vsite.schema: Housekeeping stuff. - glue/schemas/vsite.schema: Housekeeping stuff. - Updated all locales with updated/changed descriptions for the PHP stuff.

--- BlueOnyx/5209R/ui/base-vsite.mod/constructor/import_php_ini_settings.pl
+++ BlueOnyx/5209R/ui/base-vsite.mod/constructor/import_php_ini_settings.pl
@@ -1,8 +1,6 @@
 #!/usr/bin/perl -I/usr/sausalito/perl
-# $Id: import_php_ini_settings.pl, v1.1.0.4 Tue 16 Jun 2009 09:08:46 AM EDT mstauber Exp $
-# Copyright 2006-2009 Solarspeed Ltd. All rights reserved.
-# Copyright 2006-2010 Team BlueOnyx. All rights reserved.
-
+# $Id: import_php_ini_settings.pl
+#
 # This script parses php.ini and brings CODB up to date on how PHP is configured.
 # Can easily be extended to parse third party php.ini's through an optional config file.
 #
@@ -335,3 +333,37 @@ sub items_of_interest {
 $cce->bye('SUCCESS');
 exit(0);
 
+# 
+# Copyright (c) 2015 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2015 Team BlueOnyx, BLUEONYX.IT
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#	 notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#	 notice, this list of conditions and the following disclaimer in 
+#	 the documentation and/or other materials provided with the 
+#	 distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#	 contributors may be used to endorse or promote products derived 
+#	 from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
