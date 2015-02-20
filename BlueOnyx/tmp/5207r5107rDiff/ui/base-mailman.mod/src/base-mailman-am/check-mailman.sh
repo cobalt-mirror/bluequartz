54d1712177316087c0fe713117666e9c30b302c9
Michael Stauber <mstauber@blueonyx.it>
2015-02-20 20:08:00 +0000

    Cleanup/removal of /tmp directory

--- BlueOnyx/tmp/5207r5107rDiff/ui/base-mailman.mod/src/base-mailman-am/check-mailman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/bin/ps axf|/bin/grep "/usr/bin/python /usr/lib/mailman/bin/mailmanctl"|/bin/grep -v grep > /dev/null
-RETVAL=$?
-if [ $RETVAL == 0 ]; then
-                /bin/echo "is running..."
-fi
--- BlueOnyx/tmp/alpine.diff/alpine.mod/ci/application/modules/base/login/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$route['expired/:any'] = "login";
-$route['target/:any'] = "login";
-$route['logout/:any'] = "login";
-$route['usessl/:any'] = "login";
-
