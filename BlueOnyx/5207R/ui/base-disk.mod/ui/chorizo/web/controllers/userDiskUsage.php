62e0172914bf6e982f7e2f010d333e41561469f3
Michael Stauber <mstauber@blueonyx.it>
2015-05-01 20:42:02 +0000

    3.0.0-1BX08 - Converted indentation to spaces. - Modified ui/chorizo/web/controllers/serverDiskUsage.php to throw in   a Class check to make sure we're really only parsing Vsite objects for   Vsite disk usage. That makes sure we have all the required indexes.

--- BlueOnyx/5207R/ui/base-disk.mod/ui/chorizo/web/controllers/userDiskUsage.php
+++ BlueOnyx/5207R/ui/base-disk.mod/ui/chorizo/web/controllers/userDiskUsage.php
@@ -2,156 +2,156 @@
 
 class UserDiskUsage extends MX_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/userDiskUsage
-	 *	- or -  
-	 * 		http://example.com/index.php/userDiskUsage/index
-	 *	- or -
-	 * 		http://example.com/userDiskUsage/
-	 *
-	 * Past the login page this loads the page for userDiskUsage.
-	 *
-	 */
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *      http://example.com/index.php/userDiskUsage
+     *  - or -  
+     *      http://example.com/index.php/userDiskUsage/index
+     *  - or -
+     *      http://example.com/userDiskUsage/
+     *
+     * Past the login page this loads the page for userDiskUsage.
+     *
+     */
 
-	public function index() {
+    public function index() {
 
-		$CI =& get_instance();
-		
-	    // We load the BlueOnyx helper library first of all, as we heavily depend on it:
-	    $this->load->helper('blueonyx');
-	    init_libraries();
+        $CI =& get_instance();
+        
+        // We load the BlueOnyx helper library first of all, as we heavily depend on it:
+        $this->load->helper('blueonyx');
+        init_libraries();
 
-  		// Need to load 'BxPage' for page rendering:
-  		$this->load->library('BxPage');
-		$MX =& get_instance();
+        // Need to load 'BxPage' for page rendering:
+        $this->load->library('BxPage');
+        $MX =& get_instance();
 
-	    // Get $sessionId and $loginName from Cookie (if they are set):
-	    $sessionId = $CI->input->cookie('sessionId');
-	    $loginName = $CI->input->cookie('loginName');
-	    $locale = $CI->input->cookie('locale');
+        // Get $sessionId and $loginName from Cookie (if they are set):
+        $sessionId = $CI->input->cookie('sessionId');
+        $loginName = $CI->input->cookie('loginName');
+        $locale = $CI->input->cookie('locale');
 
-	    // Line up the ducks for CCE-Connection:
-	    include_once('ServerScriptHelper.php');
-		$serverScriptHelper = new ServerScriptHelper($sessionId, $loginName);
-		$cceClient = $serverScriptHelper->getCceClient();
-		$user = $cceClient->getObject("User", array("name" => $loginName));
-		$i18n = new I18n("base-disk", $user['localePreference']);
-		$system = $cceClient->getObject("System");
+        // Line up the ducks for CCE-Connection:
+        include_once('ServerScriptHelper.php');
+        $serverScriptHelper = new ServerScriptHelper($sessionId, $loginName);
+        $cceClient = $serverScriptHelper->getCceClient();
+        $user = $cceClient->getObject("User", array("name" => $loginName));
+        $i18n = new I18n("base-disk", $user['localePreference']);
+        $system = $cceClient->getObject("System");
 
-		// Make the users fullName safe for all charsets:
-		$user['fullName'] = bx_charsetsafe($user['fullName']);
+        // Make the users fullName safe for all charsets:
+        $user['fullName'] = bx_charsetsafe($user['fullName']);
 
-		// find all possible locales
-		$possibleLocales = array();
-		$possibleLocales = stringToArray($system["locales"]);
-		/*
-		 * don't show browser option for admin, because then it becomes unclear
-		 * what the system locale is.
-		 */
-		if ($serverScriptHelper->getLoginName() != "admin") {
-			$possibleLocales = array_merge(array("browser"), $possibleLocales);
-		}
+        // find all possible locales
+        $possibleLocales = array();
+        $possibleLocales = stringToArray($system["locales"]);
+        /*
+         * don't show browser option for admin, because then it becomes unclear
+         * what the system locale is.
+         */
+        if ($serverScriptHelper->getLoginName() != "admin") {
+            $possibleLocales = array_merge(array("browser"), $possibleLocales);
+        }
 
-		// Required array setup:
-		$errors = array();
-		$extra_headers = array();
+        // Required array setup:
+        $errors = array();
+        $extra_headers = array();
 
-		//-- Get Diskspace info:
-		$cceClient->setObject("User", array("refresh" => time()), "Disk", array("name" => $loginName));
+        //-- Get Diskspace info:
+        $cceClient->setObject("User", array("refresh" => time()), "Disk", array("name" => $loginName));
 
-		// get objects
-		list($user_oid) = $cceClient->find('User', array('name' => $loginName));
-		$userDisk = $cceClient->get($user_oid, 'Disk');
-		$user = $cceClient->get($user_oid);
+        // get objects
+        list($user_oid) = $cceClient->find('User', array('name' => $loginName));
+        $userDisk = $cceClient->get($user_oid, 'Disk');
+        $user = $cceClient->get($user_oid);
 
-		// get user disk information
-		$used = $userDisk["used"];
-		$available = $userDisk["quota"] * 1024;
+        // get user disk information
+        $used = $userDisk["used"];
+        $available = $userDisk["quota"] * 1024;
 
-		$overquota = 0;
-		// fix to correspond to new quota scheme, negative number means no quota set
-		// 0 means 0, and any positive number is that number
-		if ($available < 0) {
-		    $home = $cceClient->getObject(
-		                'Disk', 
-		                array('mountPoint' => $user['volume'])
-		            );
-		    $available = $home['total'] - $home['used'];
-		    $free = $available;
-		    $percentage = 0;
-		} 
-		else {
-			// calculate free space for user and if they are over quota
-			if (($available - $used) >= 0) {
-				$free = $available-$used;
-			}
-			else {
-				$overquota = 1;
-				$free = 0;
-			}
+        $overquota = 0;
+        // fix to correspond to new quota scheme, negative number means no quota set
+        // 0 means 0, and any positive number is that number
+        if ($available < 0) {
+            $home = $cceClient->getObject(
+                        'Disk', 
+                        array('mountPoint' => $user['volume'])
+                    );
+            $available = $home['total'] - $home['used'];
+            $free = $available;
+            $percentage = 0;
+        } 
+        else {
+            // calculate free space for user and if they are over quota
+            if (($available - $used) >= 0) {
+                $free = $available-$used;
+            }
+            else {
+                $overquota = 1;
+                $free = 0;
+            }
 
-			// find out percentage used
-			$percentage = round(100 * $used / $available);
-			// don't show percentages greater than 100 because it 
-			// could go way off the screen
-			if ($percentage > 100) {
-				$percentage = 100;
-			}
-		}
+            // find out percentage used
+            $percentage = round(100 * $used / $available);
+            // don't show percentages greater than 100 because it 
+            // could go way off the screen
+            if ($percentage > 100) {
+                $percentage = 100;
+            }
+        }
 
-		// convert into MB / GB, TB:
-		$available = simplify_number_diskspace($available, "kb", "2", "B");
-		$used = simplify_number_diskspace($used, "kb", "2", "B");
-		if ($used == "") {
-			$used = "0B";
-		}
-		$free = simplify_number_diskspace($free, "kb", "2", "B");
+        // convert into MB / GB, TB:
+        $available = simplify_number_diskspace($available, "kb", "2", "B");
+        $used = simplify_number_diskspace($used, "kb", "2", "B");
+        if ($used == "") {
+            $used = "0B";
+        }
+        $free = simplify_number_diskspace($free, "kb", "2", "B");
 
-		// Show over quota notification:
-		if ($overquota) {
-			$quotamessage = addTopTextField("userOverQuota", "", $i18n->getHtml("[[base-disk.overQuotaMsg]]"), "base-disk", "", "r", $i18n);
-			$errors[] = '<div class="alert dismissible alert_red"><img width="40" height="36" src="/.adm/images/icons/small/white/alarm_bell.png"><strong>' . $i18n->get("[[base-disk.userOverQuota]]") . ": " . $i18n->get("[[base-disk.overQuotaMsg]]") . '</strong></div>';
-		}
-		else {
-			$quotamessage = "";
-		}
+        // Show over quota notification:
+        if ($overquota) {
+            $quotamessage = addTopTextField("userOverQuota", "", $i18n->getHtml("[[base-disk.overQuotaMsg]]"), "base-disk", "", "r", $i18n);
+            $errors[] = '<div class="alert dismissible alert_red"><img width="40" height="36" src="/.adm/images/icons/small/white/alarm_bell.png"><strong>' . $i18n->get("[[base-disk.userOverQuota]]") . ": " . $i18n->get("[[base-disk.overQuotaMsg]]") . '</strong></div>';
+        }
+        else {
+            $quotamessage = "";
+        }
 
-	    //-- Generate page:
+        //-- Generate page:
 
-	    // Tell BxPage which module we are currently in:
-		$page_module = 'base_personalProfile';
+        // Tell BxPage which module we are currently in:
+        $page_module = 'base_personalProfile';
 
-		// Assemble page_body:
-		$BxPage = new BxPage();
+        // Assemble page_body:
+        $BxPage = new BxPage();
 
-		$page_body[] = addInputForm(
-									$i18n->get("diskUsageFor", "base-disk", array("userName" => $loginName)), 
-									array("toggle" => "#"),
-									addTextField("userDiskUsed", "", $used, "base-disk", "", "r", $i18n) .
-									addTextField("userDiskFree", "", $free, "base-disk", "", "r", $i18n) .
-									getBar("base-disk", "userDiskPercentage", $percentage, $i18n->getHtml("[[base-disk.userDiskPercentage_moreInfo]]", false, array("percentage" => $percentage, "used" =>$used, "total" => $available)),$i18n) .
-									$quotamessage,
-									"",
-									$i18n,
-									$BxPage,
-									$errors
-									);
+        $page_body[] = addInputForm(
+                                    $i18n->get("diskUsageFor", "base-disk", array("userName" => $loginName)), 
+                                    array("toggle" => "#"),
+                                    addTextField("userDiskUsed", "", $used, "base-disk", "", "r", $i18n) .
+                                    addTextField("userDiskFree", "", $free, "base-disk", "", "r", $i18n) .
+                                    getBar("base-disk", "userDiskPercentage", $percentage, $i18n->getHtml("[[base-disk.userDiskPercentage_moreInfo]]", false, array("percentage" => $percentage, "used" =>$used, "total" => $available)),$i18n) .
+                                    $quotamessage,
+                                    "",
+                                    $i18n,
+                                    $BxPage,
+                                    $errors
+                                    );
 
-		// Nice people say goodbye, or CCEd waits forever:
-		$cceClient->bye();
-		$serverScriptHelper->destructor();
+        // Nice people say goodbye, or CCEd waits forever:
+        $cceClient->bye();
+        $serverScriptHelper->destructor();
 
-		// Out with the page:
-	    $BxPage->render($page_module, $page_body);
+        // Out with the page:
+        $BxPage->render($page_module, $page_body);
 
-	}		
+    }       
 }
 /*
-Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
-Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+Copyright (c) 2015 Michael Stauber, SOLARSPEED.NET
+Copyright (c) 2015 Team BlueOnyx, BLUEONYX.IT
 All Rights Reserved.
 
 1. Redistributions of source code must retain the above copyright 
