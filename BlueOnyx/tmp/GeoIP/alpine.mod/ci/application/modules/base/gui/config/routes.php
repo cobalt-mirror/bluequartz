54f6dffa5e2ba4dab41e0028ade817245b903753
Michael Stauber <mstauber@blueonyx.it>
2015-05-27 23:09:41 +0000

    SVN Cleanup

--- BlueOnyx/tmp/GeoIP/alpine.mod/ci/application/modules/base/gui/config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$route['validation/:any'] = "validation";
-$route['gui/processing'] = "processing";
-$route['gui/processFrame'] = "processFrame";
-
--- BlueOnyx/tmp/GeoIP/alpine.mod/ci/application/views/gui_view.php
+++
@@ -1,5 +0,0 @@
-<!-- Start: gui_view.php --> 
-
-<?php echo $page_body; ?>
-
-<!-- End: gui_view.php --> 
