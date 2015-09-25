7ccc1fd8b3b07d90e7e79c440652cbd8827d4a4d
Michael Stauber <mstauber@blueonyx.it>
2015-09-25 03:38:46 +0000

    2.1.0-0BX11 - PAM fixes.

--- BlueOnyx/5207R/ui/base-console.mod/constructor/import_pam_abl_settings.pl
+++ BlueOnyx/5207R/ui/base-console.mod/constructor/import_pam_abl_settings.pl
@@ -38,9 +38,14 @@ chomp($fullbuild);
 my ($build, $model, $lang) = ($fullbuild =~ m/^build (\S+) for a (\S+) in (\S+)/);
 
 # Copy the right PAM config files into the right places:
-if (($model eq "5107R") || ($model eq "5108R") || ($model eq "5207R") || ($model eq "5208R")) {
+if (($model eq "5107R") ($model eq "5207R")) {
     if (-d "/usr/sausalito/configs/pam.d/el6") {
        system("/bin/cp /usr/sausalito/configs/pam.d/el6/* /etc/pam.d/");
+    }
+}
+if (($model eq "5108R") || ($model eq "5208R")) {
+    if (-d "/usr/sausalito/configs/pam.d/el6.64") {
+       system("/bin/cp /usr/sausalito/configs/pam.d/el6.64/* /etc/pam.d/");
     }
 }
 if ($model eq "5106R") {
