3946d7e04e94de24f03a20383f8bc193d0946aef
Michael Stauber <mstauber@blueonyx.it>
2015-09-25 02:37:30 +0000

    Removal of old pam_abl

--- BlueOnyx/5207R/utils/cce/pam_abl/gui-tools/log.c
+++
@@ -1,4 +0,0 @@
-/* $Id: log.c,v 1.1.1.1 2005/10/12 19:22:27 tagishandy Exp $ */
-#define TOOLS
-#include "../log.c"
-
--- BlueOnyx/5207R/utils/cce/pam_abl/gui-tools/rule.c
+++
@@ -1,4 +0,0 @@
-/* $Id: rule.c,v 1.1.1.1 2005/10/12 19:22:28 tagishandy Exp $ */
-#define TOOLS
-#include "../rule.c"
-
