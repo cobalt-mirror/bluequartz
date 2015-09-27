acdd69ac8401e0706cb211d6f1f22df258e5ffd0
Michael Stauber <mstauber@blueonyx.it>
2015-09-27 03:28:04 +0000

    2.0.0-1BX06 - Added glue POST-INSTALL call to fixhosts.pl (if present) to get around   the fact that our capstone does not have post-install scripts. - Replaced glue/handlers/etchosts.pl with the routines from base-vsite   glue/handlers/fixhosts.pl to make it more consistent.

--- BlueOnyx/ui/base-network.mod/glue/handlers/etchosts.pl
+++ BlueOnyx/ui/base-network.mod/glue/handlers/etchosts.pl
@@ -1,124 +1,147 @@
-#!/usr/bin/perl -w -I/usr/sausalito/perl -I.
-# $Id: etchosts.pl 259 2004-01-03 06:28:40Z shibuya $
-# Copyright 2000, 2001 Sun Microsystems, Inc., All rights reserved.
-#
+#!/usr/bin/perl -I/usr/sausalito/perl
+# $Id: etchosts.pl
 
-use Sauce::Util;
 use CCE;
 
 my $cce = new CCE;
 $cce->connectfd();
 
-# get system and network object ids:
-my ($system_oid) = $cce->find('System');
+@RealNetOids = $cce->find('Network', { 'enabled' => '1', 'real' => '1' } );
+@OtherNetOids = $cce->find('Network', { 'enabled' => '1', 'real' => '0' } );
+@SysOID = $cce->find("System");
 
-# only care about real interfaces
-my @network_oids = $cce->find('Network', { 'real' => 1 });
+($ok, $System) = $cce->get($SysOID[0]);
 
-# get system object
-# FIXME: should we bother checking the return status of gets
-my ($ok, $obj) = $cce->get($system_oid);
-if (!$ok) 
-{ 
-    $cce->bye('FAIL', '[[base-network.cantReadSystem]]');
-    exit(1);
+$hostname = $System->{'hostname'};
+$servername = $System->{'hostname'} . '.' . $System->{'domainname'};
+
+# Set up an array for all IP addresses of this box;
+@all_ips = ('127.0.0.1');
+
+$output = '# /etc/hosts' . "\n";
+$output .= '# Auto-generated file. Please put your customizations at the very end.' . "\n\n";
+$output .= '# Entries for localhost and primary IP address:' . "\n";
+$output .= '127.0.0.1' . filler('127.0.0.1') . "\t" . 'localhost' . filler('localhost') . "\t" . 'localhost.localdomain' . "\n";
+
+foreach $oid (@RealNetOids) {
+    ($ok, $obj) = $cce->get($oid);
+    $MainIpaddr = $obj->{'ipaddr'};
+    push (@all_ips, $MainIpaddr);
+    $output .= $MainIpaddr . filler($MainIpaddr) . "\t" . $hostname . filler($hostname) . "\t" . $servername . "\n";
 }
 
-# get all network objects that are real interfaces
-# @network_oids only contains the OIDs of real interfaces
-my @networks = ();
-for my $net_oid (@network_oids) 
-{
-    my ($ok, $net_obj) = $cce->get($net_oid);
-    if ($ok) 
-    { 
-        push @networks, $net_obj;
+if (scalar(@OtherNetOids) gt "1") {
+    $output .= "\n" . '# Entries for all other IP addresses bound to this server:' . "\n";
+}
+
+foreach $oid (@OtherNetOids) {
+    ($ok, $obj) = $cce->get($oid);
+    $ipaddr = $obj->{'ipaddr'};
+    if (($ipaddr ne "127.0.0.1") && ($ipaddr ne $MainIpaddr)) {
+        push (@all_ips, $ipaddr);
+        $output .= $ipaddr . filler($ipaddr) . "\t" . $hostname . filler($hostname) . "\t" . $servername . "\n";
     }
 }
 
-# munge hostname
-my ($hostname, $domainname);
-$hostname = $obj->{hostname};
-$domainname = $obj->{domainname};
-
-# create the /etc/hosts/file
-my $etchosts = <<EOT;
-# /etc/hosts
-# Auto-generated file.  Keep your customizations at the bottom of this file.
-127.0.0.1        localhost localhost.localdomain
-EOT
-
-# add all the real interfaces
-# this is probably optional
-for $net_obj (@networks) 
-{
-    my $dev = $net_obj->{device};
-    if ($net_obj->{ipaddr}) 
-    {
-        $etchosts .= $net_obj->{ipaddr} . 
-                        "\t\t${hostname}.${domainname} ${hostname}-${dev}.${domainname} ${hostname}-${dev}\n";
-    } 
-    else 
-    {
-        $etchosts .= "# $dev has not been configured.\n";
+$output .= "\n" . '# Entries for all Vsites on IP addresses of this server:' . "\n";
+foreach $ip (@all_ips) {
+    @Vsites_on_IP = $cce->find('Vsite', { 'ipaddr' => $ip } );
+    if (scalar(@Vsites_on_IP) gt "0") {
+        foreach $oid (@Vsites_on_IP) {
+            ($ok, $Vsite) = $cce->get($oid);
+            $output .= $ip . filler($ip) . "\t" . $Vsite->{'hostname'} . filler($Vsite->{'hostname'}) . "\t" . $Vsite->{'fqdn'} . "\n";
+        }
     }
 }
 
-# add end of CCE maintained /etc/hosts section
-$etchosts .= <<EOT;
-#END of auto-generated code.  Customize beneath this line.
-EOT
+$output .= "\n";
+$output .= '# The following lines are desirable for IPv6 capable hosts' . "\n";
+$output .= '::1                             localhost ip6-localhost ip6-loopback' . "\n";
+$output .= 'ff02::1                         ip6-allnodes' . "\n";
+$output .= 'ff02::2                         ip6-allrouters' . "\n";
+$output .= "\n";
+$output .= '# END of auto-generated code. Customize beneath this line.' . "\n";
 
-# update /etc/hosts
-my $fn = sub 
-    {
-        my ($fin, $fout) = (shift,shift);
-        my ($text) = (shift);
-        
-        # print out the CCE maintained section
-        print $fout $text;
+# Update /etc/hostname:
+$etc_hostname = '/etc/hostname';
+open(CONF, ">$etc_hostname") || die "Can't write to $etc_hostname!";
+print CONF "$hostname\n";
+close CONF;
 
-        # mark whether we found the end yet
-        my $flag = 0;
-        while (<$fin>) 
-        {
-            # print out customizations after the END mark
-            if ($flag) 
-            {
-                # need explicit $_ here, or perl gets confused with the file handle
-                print $fout $_; 
+# Read existing /etc/hosts to see if we need to keep modifications that the user added:
+$user_additions = '';
+$etc_hosts = '/etc/hosts';
+if (-f $etc_hosts) {
+    if (open(my $fh, '<:encoding(UTF-8)', $etc_hosts)) {
+        $found_anchor = '0';
+        while (my $row = <$fh>) {
+            chomp $row;
+            if ($row =~ /^# END of auto-generated code. Customize beneath this line./) {
+                $found_anchor = "1";
+                next;
             }
-            else 
-            {
-                # remember that the END has come
-                if (m/^#END/) { $flag = 1; }
+            if ($found_anchor eq "1") {
+                $user_additions .= $row . "\n";
             }
         }
-        return 1;
-    };
+    }
+}
 
-Sauce::Util::editfile('/etc/hosts', $fn, $etchosts);
-Sauce::Util::chmodfile(0644,'/etc/hosts');
+# Combine our output with user_additions:
+$output .= $user_additions;
+
+# Update /etc/hosts
+open(CONF, ">$etc_hosts") || die "Can't write to $etc_hosts!";
+print CONF "$output";
+close CONF;
 
 $cce->bye('SUCCESS');
 exit(0);
 
-# Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
+sub filler {
+    my ($data) = @_;
+    $ln = length($data);
+    $maxIPlen = '24';
+    $fill = $maxIPlen - $ln;
+    $spacer = '';
+    while ($fill > '0') {
+        $spacer .= " ";
+        $fill--;
+    }
+    return $spacer;
+}
+
 # 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
+# Copyright (c) 2015 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2015 Team BlueOnyx, BLUEONYX.IT
+# All Rights Reserved.
 # 
-# -Redistribution of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
+# 1. Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer.
 # 
-# -Redistribution in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation  
-# and/or other materials provided with the distribution.
+# 2. Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
 # 
-# Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-# be used to endorse or promote products derived from this software without 
-# specific prior written permission.
+# 3. Neither the name of the copyright holder nor the names of its 
+#    contributors may be used to endorse or promote products derived 
+#    from this software without specific prior written permission.
 # 
-# This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 # 
-# You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
