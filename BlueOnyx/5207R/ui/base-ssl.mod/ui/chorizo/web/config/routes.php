1e101f4e28d076bc0b5609ae65dfddb3c4d522ab
Michael Stauber <mstauber@blueonyx.it>
2015-12-06 03:18:13 +0000

    Checking in last pre-'Let's Encrypt' base-ssl.mod

---
+++ BlueOnyx/5207R/ui/base-ssl.mod/ui/chorizo/web/config/routes.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$route['ssl/siteSSL'] = "siteSSL";
+$route['ssl/createCert'] = "createCert";
+$route['ssl/uploadCert'] = "uploadCert";
+$route['ssl/caManager'] = "caManager";
