e85e133e13f7902a5b2b5aea0c41e5058b20b816
Greg Kuhnert <gkuhnert@compassnetworks.com.au>
2015-12-13 00:46:43 +0000

    2.0.0-0BX12 - Fixed: Sosreport was not generating data on 5209R

--- BlueOnyx/5207R/ui/base-support.mod/glue/sbin/sosreport.sh
+++ BlueOnyx/5207R/ui/base-support.mod/glue/sbin/sosreport.sh
@@ -4,40 +4,43 @@
 #
 #
 
-REPORTONLINE=1
-
 #
+# Do we know our model number? If not, bail out
 #
-#
-#
-#
-
-SOSVERSION=`rpm -qi sos | grep ^Version | cut -d ":" -f 2 | cut -d " " -f 2`
-SOSMAJORVERSION=`echo $SOSVERSION | cut -d "." -f 1`
 MODEL=`cat /etc/build |cut -d " " -f5|cut -d "R" -f1`
-
-#
-#
-# Create our directory, and cleanup previous sessions
-#
-#
-
-if [ ! -e /usr/sausalito/ui/web/debug/ ]
-then
-  mkdir /usr/sausalito/ui/web/debug
-else
-  tmpwatch 96 /usr/sausalito/ui/web/debug
+if [ -z "$MODEL" ]; then
+	echo "Unable to find model number"
+	exit 1
 fi
 
 #
-# Make sure we are running on a blueonyx server
+# Make sure we are running on a blueonyx server. If not, bail out
+#
+if [ ! -e /usr/sausalito/ui/web/ ]; then
+	echo "Sausalito directory structure error - Cannot find output directory"
+	exit 1
+fi
+
+#
+# Create our directory if required
 #
 
-if [ ! -e /usr/sausalito/ui/web/ ]
-then
-  echo "Sausalito directory structure error - Cannot find output directory"
-  exit 1
+if [ ! -e /usr/sausalito/ui/web/debug/ ]; then
+	mkdir /usr/sausalito/ui/web/debug
 fi
+
+#
+# Flush any old SOS-Report's from CODB:
+#
+/usr/sausalito/sbin/sosreport-helper.pl -flush=yes
+
+rm -rf /usr/sausalito/ui/web/debug/sosreport*$SERIALNUMBER.$MODEL-*
+rm -rf /usr/sausalito/ui/web/debug/$SERIALNUMBER*$MODEL*
+
+for X in `ls -F /usr/sausalito/ui/web/debug/ | grep "/$" | cut -d "/" -f 1`
+do
+	rm -rf /usr/sausalito/ui/web/debug/$X
+done
 
 #
 # Before we start, create bx.py as our plugin. No point proceeding without it!!!!
@@ -45,10 +48,6 @@ fi
 
 if [ ! -e /usr/lib/python*/site-packages/sos/plugins/bx.py ]
 then
-  cd /usr/lib/python*/site-packages/sos/plugins/
-  #echo Creating bx.py in `pwd`
-
-
 #####
 cat <<'EOF' >bx.py
 ##
@@ -74,6 +73,7 @@ class bx(sos.plugintools.PluginBase):
         return
 EOF
 #####
+mv bx.py /usr/lib/python*/site-packages/sos/plugins/
 
 fi
 
@@ -83,8 +83,6 @@ fi
 
 if [ ! -e /usr/lib/python*/site-packages/sos/plugins/admserv.py ]
 then
-  cd /usr/lib/python*/site-packages/sos/plugins/
-  #echo Creating admserv.py in `pwd`
 
 
 #####
@@ -115,114 +113,136 @@ class admserv(sos.plugintools.PluginBase):
 
 EOF
 #####
+mv admserv.py /usr/lib/python*/site-packages/sos/plugins/
 fi
+
+#
+# Get our serial number
+#
 
 SOID=`echo "find System" | /usr/sausalito/bin/cceclient | grep ^104 | cut -d " " -f 3`
 SERIALNUMBER=`echo get $SOID | /usr/sausalito/bin/cceclient | grep ^102.*serialNumber | cut -d '"' -f 2`
 
 if [ -z "$SERIALNUMBER" ]
 then
-  SERIALNUMBER='none';
+	SERIALNUMBER='none';
 fi
 
-if [ -z "$MODEL" ]
-then
-  echo "Unable to find model number"
-  exit 1
-fi
+#
+# Get a list of available plugins... and disable known bad plugins depending on our environent
+#
 
 rpm -ql sos | less | grep plugins | sed -e "s/.*plugins.//" | sed -e "s/\.py.*//" | uniq > /tmp/sosoptions.txt
 SOSREPORTOPTIONS=
 if [ -e /proc/user_beancounters ]
 then
-  grep "0:" /proc/user_beancounters
-  grep -q "0:" /proc/user_beancounters
-  if [ $? -gt 0 ]
-  then
-    for X in filesys gfs2 devicemapper
-    do
-      grep -q $X /tmp/sosoptions.txt
-      if [ $? -eq 0 ]
-      then
-        SOSREPORTOPTIONS="$SOSREPORTOPTIONS -n $X"
-      fi
-    done
-    #SOSREPORTOPTIONS="-n filesys -n gfs2 -n devicemapper"
-  fi
+	grep "0:" /proc/user_beancounters
+	grep -q "0:" /proc/user_beancounters
+	if [ $? -gt 0 ]; then
+		for X in filesys gfs2 devicemapper
+		do
+			grep -q $X /tmp/sosoptions.txt
+			if [ $? -eq 0 ]; then
+				SOSREPORTOPTIONS="$SOSREPORTOPTIONS -n $X"
+			fi
+		done
+	fi
 fi
 
-if [ $SOSMAJORVERSION -eq 2 ]
+#
+# Set some more options based on our SOSREPORT version
+#
+
+SOSMAJORVERSION=`rpm -qa sos | cut -d "-" -f 2 | cut -d "." -f 1`
+if [ $SOSMAJORVERSION -eq 1 ]
 then
-  SOSREPORTOPTIONS="$SOSREPORTOPTIONS --report"
+	SOSREPORTOPTIONS="$SOSREPORTOPTIONS --report"
 fi
+
+SOSREPORTOPTIONS="$SOSREPORTOPTIONS --tmp-dir=/usr/sausalito/ui/web/debug/"
+
+#
+# Generate our report
+#
 
 echo "Running sosreport now. This will take a while. Do not break out of the program."
-
-echo "$SOSREPORTOPTIONS"
-
-## Flushing any old SOS-Report's from CODB:
-/usr/sausalito/sbin/sosreport-helper.pl -flush=yes
-
+echo sosreport -av --batch --ticket-number=$MODEL --name=$SERIALNUMBER $SOSREPORTOPTIONS
+read me
 sosreport -av --batch --ticket-number=$MODEL --name=$SERIALNUMBER $SOSREPORTOPTIONS # 2> /dev/null >/dev/null
-ls /tmp/sosreport-$SERIALNUMBER.$MODEL-* 2>/dev/null > /dev/null
+
+ls /usr/sausalito/ui/web/debug/sosreport-$SERIALNUMBER.$MODEL-* 2>/dev/null > /dev/null
 if [ ! $? -eq 0 ]
 then
-  echo "sosreport failed to find /tmp/sosreport-$SERIALNUMBER.$MODEL-*"
-  exit 1
+	echo "sosreport failed to create a report /usr/sausalito/ui/web/debug/sosreport-$SERIALNUMBER.$MODEL-*"
+	exit 1
 fi
 
+cd /usr/sausalito/ui/web/debug
 
-rm -rf /usr/sausalito/ui/web/debug/sosreport*$SERIALNUMBER.$MODEL-* 
-rm -rf /usr/sausalito/ui/web/debug/$SERIALNUMBER*$MODEL* 
+#
+# Uncompress the report 
+#
+ls /usr/sausalito/ui/web/debug/*.bz2 2>/dev/null > /dev/null
+if [ $? -eq 0 ]; then
+	tar xfj /usr/sausalito/ui/web/debug/sosreport-$SERIALNUMBER.$MODEL*.tar.bz2 -C /usr/sausalito/ui/web/debug/
+else
+	tar xfJ /usr/sausalito/ui/web/debug/sosreport-$SERIALNUMBER.$MODEL*.tar.xz -C /usr/sausalito/ui/web/debug/
+fi
 
-for X in `ls -F /usr/sausalito/ui/web/debug/ | grep "/$" | cut -d "/" -f 1`
+#
+# Set permissions so the report can be viewed via admserv
+#
+
+for X in `find /usr/sausalito/ui/web/debug/ -type d`
 do
-  rm -rf /usr/sausalito/ui/web/debug/$X
+	chmod o+x $X
+	chmod -R o+r $X
 done
 
-mv /tmp/sosreport-$SERIALNUMBER.$MODEL-* /usr/sausalito/ui/web/debug/
-
-if [ $REPORTONLINE -eq 1 ]
-then
-  ls /usr/sausalito/ui/web/debug/*.bz2 2>/dev/null > /dev/null
-  if [ $? -eq 0 ]
-  then
-    tar xfj /usr/sausalito/ui/web/debug/sosreport-$SERIALNUMBER.$MODEL*.tar.bz2 -C /usr/sausalito/ui/web/debug/
-  else
-    tar xfJ /usr/sausalito/ui/web/debug/sosreport-$SERIALNUMBER.$MODEL*.tar.xz -C /usr/sausalito/ui/web/debug/
-  fi
-
-  for X in `find /usr/sausalito/ui/web/debug/ -type d`
-  do
-    chmod o+x $X
-    chmod -R o+r $X
-  done
-  #chmod -R o+r /usr/sausalito/ui/web/debug/$SERIALNUMBER.$MODEL*
-fi
-
-for X in `ls -F /usr/sausalito/ui/web/debug/ | grep -v "/$" | cut -d "/" -f 1 | grep -v ^html`
+#
+# Generate a mini index file - Files first
+#
+for X in `ls -F /usr/sausalito/ui/web/debug/ | grep -v "/$" | sed -e "s#/.*##" -e "s#*##" | grep -v html$`
 do
-  echo "<a href=\"$X\">$X</a><br>"
+	echo "<a href=\"$X\">$X</a><br>"
 done >> /usr/sausalito/ui/web/debug/$SERIALNUMBER-$MODEL.html
+
+#
+# Generate a mini index file - subdirectories next
+#
 
 for Y in `ls -F /usr/sausalito/ui/web/debug/ | grep "/$" | cut -d "/" -f 1`
 do
-  echo "<a href=\"$Y/sos_reports/sosreport.html\">$Y/sos_reports/sosreport.html</a><br>"
+	if [ -e "$Y/sos_reports/sos.html" ]; then
+		cp -p "$Y/sos_reports/sos.html" "$Y/sos_reports/sosreport.html"
+	fi
+ 	 echo "<a href=\"$Y/sos_reports/sosreport.html\">$Y/sos_reports/sosreport.html</a><br>"
 done >> /usr/sausalito/ui/web/debug/$SERIALNUMBER-$MODEL.html
 
-for Z in `ifconfig  | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1 | grep -v 127.0.0.1 | head -1`
+#
+# Generate a list of valid sosreport url's - later we will send this to newlinq
+#
+for Z in `ifconfig  | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep -v "^255\|^0" | sort | uniq | grep -v 127.0.0.1`
 do
-  echo "http://$Z:444/debug/$SERIALNUMBER-$MODEL.html"
+	echo "http://$Z:444/debug/$SERIALNUMBER-$MODEL.html"
 done
-# | mail -s "Blueonyx Debug Submission $MODEL" mstauber@blueonyx.it
 
-# Updating CODB with the new SOS-Report:
+#
+# Update CODB with the new SOS-Report:
+#
 /usr/sausalito/sbin/sosreport-helper.pl -report=yes -internal=$Y -external=/debug/$SERIALNUMBER-$MODEL.html
 
+#
 # Fix permissions:
+#
 /bin/chmod -R 755 /usr/sausalito/ui/web/debug
 
-rm yum_debug_dump*
+
+#
+# Cleanup
+#
+rm yum_debug_dump* 2>/dev/null
+cd - 2> /dev/null
 
 # 
 # Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
