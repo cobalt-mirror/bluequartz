60beb186ac0d4a1211e00cf5ce9eb949303165a1
Michael Stauber <mstauber@blueonyx.it>
2016-06-14 23:49:06 +0000

    2.0.0-0BX09 - Modified constructor/configure_mailman.pl to create /var/lock/mailman   if need be.

--- BlueOnyx/5207R/ui/base-mailman.mod/constructor/configure_mailman.pl
+++ BlueOnyx/5207R/ui/base-mailman.mod/constructor/configure_mailman.pl
@@ -18,94 +18,99 @@ if (@OIDS) {
     my $configured = $obj->{configured};
     my $admin_pw = $obj->{enabled};
 
-	# If MailMan hasn't been configured yet, do it now:
-	if ($configured ne "1") {
+    if (-d '/var/lock/mailman') {
+        system("mkdir /var/lock/mailman");
+        system("chown mailman:mailman /var/lock/mailman");
+    }
 
-	    # Get FQDN:
-	    my $myhost = `/bin/hostname -s`;
-	    chomp ($myhost);
-	    if ( $myhost eq "" ) {
-    	        $myhost = "localhost";
-	    }
-	    my $mydomain = `/bin/hostname -d`;
-	    chomp ($mydomain);
-	    if ( $mydomain eq "" ) {
-    	        $mydomain = "localdomain";
-	    }
-	    $fqdn = $myhost . "." . "$mydomain";
+    # If MailMan hasn't been configured yet, do it now:
+    if ($configured ne "1") {
 
-	    # Generate random admin password for MailMan:
-	    my $admin_pw = &generate_random_pass(11);
+        # Get FQDN:
+        my $myhost = `/bin/hostname -s`;
+        chomp ($myhost);
+        if ( $myhost eq "" ) {
+                $myhost = "localhost";
+        }
+        my $mydomain = `/bin/hostname -d`;
+        chomp ($mydomain);
+        if ( $mydomain eq "" ) {
+                $mydomain = "localdomain";
+        }
+        $fqdn = $myhost . "." . "$mydomain";
 
-	    # Make sure there is no mailman list already:
-	    system("/usr/lib/mailman/bin/rmlist -a mailman > /dev/null 2>&1");
-	    sleep(3);
+        # Generate random admin password for MailMan:
+        my $admin_pw = &generate_random_pass(11);
 
-	    # Generate initial mandatory mailman list:
-	    system("/usr/lib/mailman/bin/newlist -q --urlhost=$fqdn --emailhost=$fqdn mailman admin\@$fqdn $admin_pw >/dev/null 2>&1");
-	    sleep(3);
+        # Make sure there is no mailman list already:
+        system("/usr/lib/mailman/bin/rmlist -a mailman > /dev/null 2>&1");
+        sleep(3);
 
-	    # Create default alias-list for mailman:
-	    ${list} = "mailman";
-	    my %aliases = (
-    		$list => "\"|/usr/lib/mailman/mail/mailman post ${list}\"",
-    		$list.'-admin' => "\"|/usr/lib/mailman/mail/mailman post ${list}\"",
-    		$list.'-bounces' => "\"|/usr/lib/mailman/mail/mailman bounces ${list}\"",
-    		$list.'-confirm' => "\"|/usr/lib/mailman/mail/mailman confirm ${list}\"",
-    		$list.'-join' => "\"|/usr/lib/mailman/mail/mailman join ${list}\"",
-    		$list.'-leave' => "\"|/usr/lib/mailman/mail/mailman leave ${list}\"",
-    		$list.'-owner' => "\"|/usr/lib/mailman/mail/mailman owner ${list}\"",
-    		$list.'-request' => "\"|/usr/lib/mailman/mail/mailman request ${list}\"",
-    		$list.'-subscribe' => "\"|/usr/lib/mailman/mail/mailman subscribe ${list}\"",
-    		$list.'-unsubscribe' => "\"|/usr/lib/mailman/mail/mailman unsubscribe ${list}\"",
-	    );
+        # Generate initial mandatory mailman list:
+        system("/usr/lib/mailman/bin/newlist -q --urlhost=$fqdn --emailhost=$fqdn mailman admin\@$fqdn $admin_pw >/dev/null 2>&1");
+        sleep(3);
 
-	    # Update /etc/mail/aliases.mailman
-	    my $ok = Sauce::Util::editfile('/etc/mail/aliases.mailman',
-	      \&Sauce::Util::replace_unique_entries, $oid, \%aliases);
+        # Create default alias-list for mailman:
+        ${list} = "mailman";
+        my %aliases = (
+            $list => "\"|/usr/lib/mailman/mail/mailman post ${list}\"",
+            $list.'-admin' => "\"|/usr/lib/mailman/mail/mailman post ${list}\"",
+            $list.'-bounces' => "\"|/usr/lib/mailman/mail/mailman bounces ${list}\"",
+            $list.'-confirm' => "\"|/usr/lib/mailman/mail/mailman confirm ${list}\"",
+            $list.'-join' => "\"|/usr/lib/mailman/mail/mailman join ${list}\"",
+            $list.'-leave' => "\"|/usr/lib/mailman/mail/mailman leave ${list}\"",
+            $list.'-owner' => "\"|/usr/lib/mailman/mail/mailman owner ${list}\"",
+            $list.'-request' => "\"|/usr/lib/mailman/mail/mailman request ${list}\"",
+            $list.'-subscribe' => "\"|/usr/lib/mailman/mail/mailman subscribe ${list}\"",
+            $list.'-unsubscribe' => "\"|/usr/lib/mailman/mail/mailman unsubscribe ${list}\"",
+        );
+
+        # Update /etc/mail/aliases.mailman
+        my $ok = Sauce::Util::editfile('/etc/mail/aliases.mailman',
+          \&Sauce::Util::replace_unique_entries, $oid, \%aliases);
         
-	    if (!$ok || ($ok eq 'FAIL')) {
-	      $cce->warn("Mail-alias-already-taken");
-	      $cce->bye("FAIL");
-	      exit(1);
-	    }
+        if (!$ok || ($ok eq 'FAIL')) {
+          $cce->warn("Mail-alias-already-taken");
+          $cce->bye("FAIL");
+          exit(1);
+        }
 
-	    # Run newaliases:
-	    system("/usr/bin/newaliases >/dev/null 2>&1");
+        # Run newaliases:
+        system("/usr/bin/newaliases >/dev/null 2>&1");
 
-	    # Update CCE with the new info so that this snippet only runs once:
-	    $cce->set($OIDS[0], 'MailListStatus', { 
-		'configured' => "1",
-		'enabled' => "1", 
-		'admin_pw' => $admin_pw, 
-		'force_update' => time()
-		});
+        # Update CCE with the new info so that this snippet only runs once:
+        $cce->set($OIDS[0], 'MailListStatus', { 
+        'configured' => "1",
+        'enabled' => "1", 
+        'admin_pw' => $admin_pw, 
+        'force_update' => time()
+        });
 
-	    # While we're turning MailMan on, we also tell ActiveMonitor
-	    # to start monitoring this component:		
-	    @amOIDS = $cce->find('ActiveMonitor');
-	    if (@amOIDS) {
-		$cce->set($amOIDS[0], 'MAILMAN', { 
-		'enabled' => "1" 
-		});
-	    }
-	}
+        # While we're turning MailMan on, we also tell ActiveMonitor
+        # to start monitoring this component:       
+        @amOIDS = $cce->find('ActiveMonitor');
+        if (@amOIDS) {
+        $cce->set($amOIDS[0], 'MAILMAN', { 
+        'enabled' => "1" 
+        });
+        }
+    }
 }
 
 $cce->bye('SUCCESS');
 
 # This function generates random password of a given length
 sub generate_random_pass {
-	my $length_of_random_pass=shift;
+    my $length_of_random_pass=shift;
 
-	my @chars=('a'..'z','A'..'Z','0'..'9','_', ".");
-	my $random_string;
-	foreach (1..$length_of_random_pass) {
-		# rand @chars will generate a random 
-		# number between 0 and scalar @chars
-		$random_string.=$chars[rand @chars];
-	}
-	return $random_string;
+    my @chars=('a'..'z','A'..'Z','0'..'9','_', ".");
+    my $random_string;
+    foreach (1..$length_of_random_pass) {
+        # rand @chars will generate a random 
+        # number between 0 and scalar @chars
+        $random_string.=$chars[rand @chars];
+    }
+    return $random_string;
 }
 
 exit 0;
@@ -117,16 +122,16 @@ exit 0;
 # All Rights Reserved.
 # 
 # 1. Redistributions of source code must retain the above copyright 
-#	 notice, this list of conditions and the following disclaimer.
+#    notice, this list of conditions and the following disclaimer.
 # 
 # 2. Redistributions in binary form must reproduce the above copyright 
-#	 notice, this list of conditions and the following disclaimer in 
-#	 the documentation and/or other materials provided with the 
-#	 distribution.
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
 # 
 # 3. Neither the name of the copyright holder nor the names of its 
-#	 contributors may be used to endorse or promote products derived 
-#	 from this software without specific prior written permission.
+#    contributors may be used to endorse or promote products derived 
+#    from this software without specific prior written permission.
 # 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
