5e5ee4bc3521b8fdd6d4c06d45dbd400c5d97194
Michael Stauber <mstauber@blueonyx.it>
2016-06-22 16:34:55 +0000

    2.0.0-0BX21 - Fixed glue/handlers/writeback_blueOnyx_conf.pl - See http://kx.cloudingenium.com/linux/apache-web-server/solve-apache   -error-htaccess-option-multiviews-allowed/

--- BlueOnyx/5207R/platform/base-apache.mod/glue/handlers/writeback_blueOnyx_conf.pl
+++ BlueOnyx/5207R/platform/base-apache.mod/glue/handlers/writeback_blueOnyx_conf.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -I/usr/sausalito/perl -I.
-# $Id: writeback_blueOnyx_conf.pl Fri 12 Jun 2009 07:10:43 PM CEST mstauber $
+# $Id: writeback_blueOnyx_conf.pl
 #
 # This handler is responsible for updating /etc/httpd/conf.d/blueonyx.conf
 
@@ -31,42 +31,51 @@ sub edit_blueonyx {
 
     my @Options = ();
     if ($web->{Options_Indexes} == "1") {
-	push(@Options, "Indexes");
+        push(@Options, "Indexes");
     }
     if ($web->{Options_SymLinksIfOwnerMatch} == "1") {
-    	push(@Options, "SymLinksIfOwnerMatch");
+        push(@Options, "SymLinksIfOwnerMatch");
     }
     if ($web->{Options_FollowSymLinks} == "1") {
-	push(@Options, "FollowSymLinks");
+        push(@Options, "FollowSymLinks");
     }
     if ($web->{Options_Includes} == "1") {
-	push(@Options, "Includes");
+        push(@Options, "Includes");
     }
     if ($web->{Options_MultiViews} == "1") {
-    	push(@Options, "MultiViews");
+        push(@Options, "MultiViews");
     }
     if ($web->{Options_All} == "1") {
-	push(@Options, "All");
+        push(@Options, "All");
     }
 
     my @AllowOverride = ();
     if ($web->{AllowOverride_AuthConfig} == "1") {
-	push(@AllowOverride, "AuthConfig");
+        push(@AllowOverride, "AuthConfig");
     }
     if ($web->{AllowOverride_Indexes} == "1") {
-    	push(@AllowOverride, "Indexes");
+        push(@AllowOverride, "Indexes");
     }
     if ($web->{AllowOverride_Limit} == "1") {
-	push(@AllowOverride, "Limit");
+        push(@AllowOverride, "Limit");
     }
     if ($web->{AllowOverride_FileInfo} == "1") {
-    	push(@AllowOverride, "FileInfo");
+        push(@AllowOverride, "FileInfo");
     }
     if ($web->{AllowOverride_Options} == "1") {
-	push(@AllowOverride, "Options");
+        if ($web->{AllowOverride_All} == "1") {
+        }
+        else {
+            push(@AllowOverride, "Options");
+        }
     }
     if ($web->{AllowOverride_All} == "1") {
-	push(@AllowOverride, "All");
+        if ($web->{Options_MultiViews} == "1") {
+            push(@AllowOverride, "Options=All,MultiViews");
+        }
+        else {
+            push(@AllowOverride, "All");
+        }
     }
 
     $out_options = join(" ", @Options);
@@ -104,3 +113,37 @@ sub edit_blueonyx {
 $cce->bye('SUCCESS');
 exit(0);
 
+# 
+# Copyright (c) 2016 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2016 Team BlueOnyx, BLUEONYX.IT
+# All Rights Reserved.
+# 
+# 1. Redistributions of source code must retain the above copyright 
+#     notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright 
+#     notice, this list of conditions and the following disclaimer in 
+#     the documentation and/or other materials provided with the 
+#     distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its 
+#     contributors may be used to endorse or promote products derived 
+#     from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
