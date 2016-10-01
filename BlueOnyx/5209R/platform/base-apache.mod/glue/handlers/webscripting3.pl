ce3e1351cd72ee62816cf60c4037049b0277b1bd
Michael Stauber <mstauber@blueonyx.it>
2016-10-01 20:01:53 +0000

    2.1.1-0BX48 - Improved PHP-FPM implementation with a conditional FCGI call. - Added glue/sbin/phptoggle.pl - Modified glue/Makefile - Modified glue/handlers/webscripting.pl - Modified glue/handlers/webscripting2.pl - Modified glue/handlers/webscripting3.pl

--- BlueOnyx/5209R/platform/base-apache.mod/glue/handlers/webscripting3.pl
+++ BlueOnyx/5209R/platform/base-apache.mod/glue/handlers/webscripting3.pl
@@ -404,7 +404,8 @@ sub edit_vhost
                         }
                         # Replace double slash with single slash:
                         $VsiteDocRoot =~ s/\/\//\//g;
-                        $script_conf .= "ProxyPassMatch ^/(.*\\.php(/.*)?)\$ fcgi://127.0.0.1:$fpmPort$VsiteDocRoot\n";
+                        $script_conf .= "RewriteCond $VsiteDocRoot%{REQUEST_FILENAME} -f\n";
+                        $script_conf .= "RewriteRule ^/(.*\.php(/.*)?)\$ fcgi://127.0.0.1:$fpmPort$VsiteDocRoot\$1 [L,P]\n";
                 }
                 # Handle 'regular' PHP via DSO:
                 else {
