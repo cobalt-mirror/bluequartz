1cb305701c19a519302250344cbb17770f5ef02e
Michael Stauber <mstauber@blueonyx.it>
2017-01-26 19:41:25 +0000

    Back to the unified base-mysql.mod for all 520XR

--- BlueOnyx/5209R/ui/base-mysql.mod/ui/chorizo/web/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$route['mysql/mysqlserver/:any'] = "mysqlserver";
-$route['mysql/mysqlconfig'] = "mysqlconfig";
-$route['mysql/vsiteMySQL'] = "vsiteMySQL";
-$route['mysql/mysql_amdetails'] = "mysql_amdetails";
-
