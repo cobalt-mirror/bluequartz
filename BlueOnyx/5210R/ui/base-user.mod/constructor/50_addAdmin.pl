5740ab28fcad8ab5348cada7cc02f6b9364b4c90
Michael Stauber <mstauber@blueonyx.it>
2017-11-06 21:17:56 +0000

    2.0.0-0BX27 - Modified constructors to use cce->update() instead of cce->set().

--- BlueOnyx/5210R/ui/base-user.mod/constructor/50_addAdmin.pl
+++ BlueOnyx/5210R/ui/base-user.mod/constructor/50_addAdmin.pl
@@ -16,46 +16,37 @@ $cce->connectuds();
 my $i18n = new I18n;
 
 # make sure the default directory for system users exists
-if (! -d '/home/users')
-{
+if (! -d '/home/users') {
     mkdir('/home/users', 0755);
 }
 
 # make sure /etc/shadow exists
-if (! -f '/etc/shadow')
-{
+if (! -f '/etc/shadow') {
     system('/usr/sbin/pwconv');
 }
  
 # check if admin exists already
-my (@oids) = $cce->find('User', { name => 'admin' });
+my (@oids) = $cce->find('User', { 'name' => 'admin' });
 my $success;
 
-if($#oids >= 0) 
-{
+if ($#oids >= 0) {
     # make sure admin has the systemAdministrator
     # field set
     my ($ok, $obj) = $cce->get($oids[0]);
 
     # add systemAdministrator right if adminUser right does not exist
-    if(!$obj->{systemAdministrator}) 
-    {   
+    if(!$obj->{'systemAdministrator'}) {
         # reset to systemAdministrator if adminUser not found
-	($success) = $cce->set($oids[0], '', 
-			       { 
-				   systemAdministrator => 1
-				   });
+        ($success) = $cce->update($oids[0], '', { 'systemAdministrator' => 1 });
         
         # turn su acces back on if resetting admin to sys_admin
-        ($success) = $cce->set($oids[0], 'RootAccess', { 'enabled' => 1 });
+        ($success) = $cce->update($oids[0], 'RootAccess', { 'enabled' => 1 });
     }
 
     # always reenable Shell for now
-    ($success) = $cce->set($oids[0], 'Shell',
-                                { 'enabled' => 1 });
+    ($success) = $cce->update($oids[0], 'Shell', { 'enabled' => 1 });
 }
-else
-{
+else {
     $i18n->setLocale(I18n::i18n_getSystemLocale($cce));
 
     # If no 'admin' account exists, then it is likely that the group 'site-adm'
@@ -71,52 +62,65 @@ else
     # create with systemAdministrator set at first
     ($success) = $cce->create('User', 
             {
-                fullName => $i18n->get('administrator', {}, 'base-user'),
-                localePreference => $locale,
-                name => 'admin',
-                password => 'blueonyx',
-                stylePreference => 'BlueOnyx',
-                systemAdministrator => 1,
-                volume => $Base::HomeDir::HOME_ROOT
+                'fullName' => $i18n->get('administrator', {}, 'base-user'),
+                'localePreference' => $locale,
+                'name' => 'admin',
+                'password' => 'blueonyx',
+                'stylePreference' => 'BlueOnyx',
+                'systemAdministrator' => 1,
+                'volume' => $Base::HomeDir::HOME_ROOT
             });
 
     # turn on telnet and su access by default
-    if ($success)
-    {
+    if ($success) {
         my $oid = $cce->oid();
-        ($success) = $cce->set($oid, 'Shell', 
-                        { 'enabled' => 1 });
+        ($success) = $cce->update($oid, 'Shell', { 'enabled' => 1 });
        
         # enable su access
-        ($success) = $cce->set($oid, 'RootAccess', { 'enabled' => 1 });
+        ($success) = $cce->update($oid, 'RootAccess', { 'enabled' => 1 });
     }
 }
 
 $cce->bye();
 
 # failed?
-if(!$success) 
-{
-  exit 1;
+if(!$success) {
+    exit 1;
 }
 
 exit 0;
-# Copyright (c) 2003 Sun Microsystems, Inc. All  Rights Reserved.
 # 
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
+# Copyright (c) 2014 Michael Stauber, SOLARSPEED.NET
+# Copyright (c) 2014 Team BlueOnyx, BLUEONYX.IT
+# Copyright (c) 2003 Sun Microsystems, Inc. 
+# All Rights Reserved.
 # 
-# -Redistribution of source code must retain the above copyright notice, 
-# this list of conditions and the following disclaimer.
+# 1. Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer.
 # 
-# -Redistribution in binary form must reproduce the above copyright notice, 
-# this list of conditions and the following disclaimer in the documentation  
-# and/or other materials provided with the distribution.
+# 2. Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in 
+#    the documentation and/or other materials provided with the 
+#    distribution.
 # 
-# Neither the name of Sun Microsystems, Inc. or the names of contributors may 
-# be used to endorse or promote products derived from this software without 
-# specific prior written permission.
+# 3. Neither the name of the copyright holder nor the names of its 
+#    contributors may be used to endorse or promote products derived 
+#    from this software without specific prior written permission.
 # 
-# This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
 # 
-# You acknowledge that  this software is not designed or intended for use in the design, construction, operation or maintenance of any nuclear facility.
+# You acknowledge that this software is not designed or intended for 
+# use in the design, construction, operation or maintenance of any 
+# nuclear facility.
+# 
