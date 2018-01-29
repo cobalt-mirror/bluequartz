fd6cac582d196822eee90270b5280f6facb76a88
Michael Stauber <mstauber@blueonyx.it>
2018-01-29 01:34:39 +0000

    4.0.0-0BX45 - Modified Hotfixes and swatch.sh for our new IPv6 provisions.

--- BlueOnyx/5210R/utils/swatch/hotfixes.sh
+++ BlueOnyx/5210R/utils/swatch/hotfixes.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# Make sure 'ifup-routes' and 'ifup' have our handler provisions:
+/usr/sausalito/constructor/base/network/10_fix_ifup.pl
+
+# Make sure our IPs are all bound and our routes are all OK:
+/usr/sausalito/handlers/base/network/change_route.pl -c 2 >/dev/null 2>&1 || :
+
 # Fix PHP Session dir GID if needed:
 if [ -d "/var/lib/php/session" ];then
-	SESSPERMS=`ls -la /var/lib/php|grep session|awk '{print $1}'`
-	if [ $SESSPERMS != "drwxrwxrwx" ];then
-		chmod 777 /var/lib/php/session
-	fi
+    SESSPERMS=`ls -la /var/lib/php|grep session|awk '{print $1}'`
+    if [ $SESSPERMS != "drwxrwxrwx" ];then
+        chmod 777 /var/lib/php/session
+    fi
 fi
 
 # Remove the OS supplied YUM autoupdater (if present), as we bring our own:
 if [ -f /etc/cron.daily/yum-autoupdate ] ; then
-	rm -f /etc/cron.daily/yum-autoupdate
+    rm -f /etc/cron.daily/yum-autoupdate
 fi
 
 # The CD-Installer of BlueOnyx brings /usr/bin/fix-httpd-log-dir aboard, but
 # it might not be executable:
 if [ -f /usr/bin/fix-httpd-log-dir ]; then
-	chmod 755 /usr/bin/fix-httpd-log-dir
+    chmod 755 /usr/bin/fix-httpd-log-dir
 fi
 
 # While we are at it, delete the default CentOS welcome page:
@@ -49,14 +55,14 @@ fi
 
 # Fix Imagetragick issue:
 if [ -f /etc/ImageMagick/policy.xml ];then
-	ITRAGIC=`cat /etc/ImageMagick/policy.xml |grep TEXT|wc -l`
-	# We need to apply the fix:
-	if [ $ITRAGIC -eq 0 ]; then
-		# Do we have a good copy? We should!
-		if [ -f /etc/ImageMagick/policy.xml.fixed ];then
-			cp /etc/ImageMagick/policy.xml.fixed /etc/ImageMagick/policy.xml
-		fi
-	fi
+    ITRAGIC=`cat /etc/ImageMagick/policy.xml |grep TEXT|wc -l`
+    # We need to apply the fix:
+    if [ $ITRAGIC -eq 0 ]; then
+        # Do we have a good copy? We should!
+        if [ -f /etc/ImageMagick/policy.xml.fixed ];then
+            cp /etc/ImageMagick/policy.xml.fixed /etc/ImageMagick/policy.xml
+        fi
+    fi
 fi
 
 # CentOS 7 Snafu-fix:
