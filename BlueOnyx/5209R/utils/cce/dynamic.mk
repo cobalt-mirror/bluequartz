b52c465252d3dfe084ff5cc37f7bcb6168644216
Michael Stauber <mstauber@blueonyx.it>
2018-05-11 19:58:16 +0000

    CentOS 7.5 version number bump to fix /etc/pam.d

--- BlueOnyx/5209R/utils/cce/dynamic.mk
+++ BlueOnyx/5209R/utils/cce/dynamic.mk
@@ -1,7 +1,7 @@
-CCE_TOPDIR=/home/devel/BlueOnyx/trunk/BlueOnyx/5210R/utils/cce
+CCE_TOPDIR=/home/devel/BlueOnyx/trunk/BlueOnyx/5209R/utils/cce
 CCE_V_MAJ=0
 CCE_V_MIN=99
-CCE_V_PATCH=2
-CCE_VERSION=0.99.2
+CCE_V_PATCH=3
+CCE_VERSION=0.99.3
 include $(CCE_TOPDIR)/rules.mk
 CFLAGS += -fPIC
