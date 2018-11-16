b0b8c5f26d0883bc1aa6ad2d3f4d2f7359fec047
Michael Stauber <mstauber@blueonyx.it>
2018-11-16 20:09:00 +0000

    Removed deprecated modules

--- BlueOnyx/5210R/platform/base-java.mod/ui/chorizo/web/config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$route['java/tomcatmanager/'] = "tomcatmanager";
-$route['java/java_amdetails/'] = "java_amdetails";
-$route['java/warList/'] = "warList";
-$route['java/warAdd/'] = "warAdd";
